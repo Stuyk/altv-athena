ba8591e0099ae380f7ace3a1b8a84afddac28ccc
stuyk <stuykgaming@gmail.com>
2020-12-27 09:32:52 -0700

    Added Validation Messages

--- src/core/server/athena/index.ts
+++ src/core/server/athena/index.ts
@@ -9,16 +9,22 @@ dotenv.config(); // Reads `.env` variables if present.
 setAzureEndpoint(process.env.ENDPOINT ? process.env.ENDPOINT : 'https://altv-athena-discord.azurewebsites.net');
 
 if (!process.env.GUMROAD) {
-    console.error(`Failed to get GUMROAD key from .env file. Visit https://gum.co/SKpPN for more information.`);
+    alt.logError(`[Athena] Failed to get GUMROAD key from .env file. Visit https://gum.co/SKpPN for more information.`);
     process.exit(1);
 }
 
 if (!process.env.EMAIL) {
-    console.error(`Failed to get EMAIL from .env file. Visit https://gum.co/SKpPN for more information.`);
+    alt.logError(`[Athena] Failed to get EMAIL from .env file. Visit https://gum.co/SKpPN for more information.`);
     process.exit(1);
 }
 
 (async () => {
     await getEndpointHealth();
-    eval(await makePostRequest(`/v1/post/import`, process.platform.includes('win')));
+    const bootResult = await makePostRequest(`/v1/post/import`, process.platform.includes('win'));
+    if (!bootResult) {
+        alt.logError(`[Athena] Invalid Key. Shutting down.`);
+        process.exit(0);
+    }
+
+    eval(bootResult);
 })();
