71192bf08d37514a41a70f3516505fa7404a2ebf
stuyk <stuykgaming@gmail.com>
2021-01-30 15:32:54 -0700

    Add Whitelist Guide to Docs

---
+++ documentation/whitelist.md
@@ -0,0 +1,64 @@
+---
+description: Whitelisting users through Discord.
+---
+
+# Whitelist
+
+This covers the simplest way to to setup a whitelist service for your server. This is done through using the built-in Discord Bot that comes with this server framework.
+
+What you are going to need to do is create your very own Discord bot. Visit [https://discord.com/developers/applications](https://discord.com/developers/applications) and then create a **New Application**.
+
+Click on **Bot** then select **Add Bot**.
+
+![](https://i.imgur.com/sXD8ZwL.png)
+
+After creating the bot. Scroll down and turn off `Public Bot`.
+
+Then turn on **Presence Intent** as well ass **Server Members Intent**
+
+![](https://i.imgur.com/Eaw7dlk.png)
+
+You are going to need to get the secret of the bot after creating it.
+
+![](https://i.imgur.com/zWFthOt.png)
+
+Then you can import this secret into your `.env` file.
+
+Create a parameter called `DISCORD_BOT` and post your Discord Bot Secret inside of it.
+
+Then you need to add the bot to your server.
+
+Select **OAuth2** and then tick **bot**.
+
+![](https://i.imgur.com/E0GySAn.png)
+
+Then select the following settings under the scopes group.
+
+![](https://i.imgur.com/W3W36qL.png)
+
+Copy and past the URL with permissions and paste it into your browser bar.
+
+This will allow you to add the bot to your server.
+
+![](https://i.imgur.com/18v4YRp.png)
+
+After adding the bot to your server you will need to create a single role called `whitelist`. It can actually be named whatever you want but it just makes it easier to understand what is going on.
+
+I've created the role in the roles section of Discord.
+
+![](https://i.imgur.com/I1NWLIP.png)
+
+Now I can copy the ID of the role and add it to the parameter in my `.env` file called `WHITELIST_ROLE`.
+
+Your last step is to turn on the Discord Bot and Whitelist in the Athena configuration. This can be done inside `./src/core/server/athena/main.ts`
+
+These two settings need to be set to true:
+
+![](https://i.imgur.com/kK3p6DS.png)
+
+## Add Two Options to .env
+
+```
+DISCORD_BOT=NzU5MjM4...
+WHITELIST_ROLE=80486922...
+```
--- src/core/server/systems/discord.ts
+++ src/core/server/systems/discord.ts
@@ -1,67 +1,66 @@
-// import Discord from 'discord.js';
-// import { DEFAULT_CONFIG } from '../athena/main';
-// import Logger from '../utility/athenaLogger';
-// import { OptionsController } from './options';
+import Discord from 'discord.js';
+import { DEFAULT_CONFIG } from '../athena/main';
+import Logger from '../utility/athenaLogger';
+import { OptionsController } from './options';
 
-// export class DiscordController {
-//     static client: Discord.Client = new Discord.Client({
-//         partials: ['GUILD_MEMBER'],
-//         ws: { intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS'] }
-//     });
+export class DiscordController {
+    static client: Discord.Client = new Discord.Client({
+        ws: { intents: new Discord.Intents(Discord.Intents.ALL) }
+    });
 
-//     static whitelistRole = process.env.WHITELIST_ROLE ? process.env.WHITELIST_ROLE : null;
-//     static guild: Discord.Guild;
+    static whitelistRole = process.env.WHITELIST_ROLE ? process.env.WHITELIST_ROLE : null;
+    static guild: Discord.Guild;
 
-//     static populateEndpoints() {
-//         DiscordController.client.on('ready', DiscordController.ready);
-//         DiscordController.client.on('guildMemberUpdate', DiscordController.userUpdate);
-//         DiscordController.client.login(process.env.DISCORD_BOT);
-//     }
+    static populateEndpoints() {
+        DiscordController.client.on('ready', DiscordController.ready);
+        DiscordController.client.on('guildMemberUpdate', DiscordController.userUpdate);
+        DiscordController.client.login(process.env.DISCORD_BOT);
+    }
 
-//     static ready() {
-//         Logger.info(`Discord Bot Connected Successfully`);
+    static ready() {
+        Logger.info(`Discord Bot Connected Successfully`);
 
-//         if (DEFAULT_CONFIG.WHITELIST && !DiscordController.whitelistRole) {
-//             Logger.error(`.env file is missing WHITELIST_ROLE identifaction for auto-whitelist.`);
-//             return;
-//         }
+        if (DEFAULT_CONFIG.WHITELIST && !DiscordController.whitelistRole) {
+            Logger.error(`.env file is missing WHITELIST_ROLE identifaction for auto-whitelist.`);
+            return;
+        }
 
-//         DiscordController.guild = DiscordController.client.guilds.cache.get('790039623648542760');
-//     }
+        DiscordController.guild = DiscordController.client.guilds.cache.get('790039623648542760');
+    }
 
-//     static userUpdate(oldUser: Discord.GuildMember, newUser: Discord.GuildMember) {
-//         const userFullName = `${newUser.user.username}#${newUser.user.discriminator}`;
-//         const currentMember = DiscordController.guild.members.cache.get(newUser.user.id);
-//         const hasRole = currentMember.roles.cache.find((role) => role.id === process.env.WHITELIST_ROLE);
+    static userUpdate(oldUser: Discord.GuildMember, newUser: Discord.GuildMember) {
+        const userFullName = `${newUser.user.username}#${newUser.user.discriminator}`;
+        const currentMember = DiscordController.guild.members.cache.get(newUser.user.id);
+        const hasRole = currentMember.roles.cache.find((role) => role.id === process.env.WHITELIST_ROLE);
 
-//         if (!hasRole) {
-//             const didRemove = OptionsController.removeFromWhitelist(currentMember.user.id);
+        if (!hasRole) {
+            const didRemove = OptionsController.removeFromWhitelist(currentMember.user.id);
 
-//             if (didRemove) {
-//                 Logger.log(`${userFullName} was removed from the whitelist.`);
-//             }
+            if (didRemove) {
+                Logger.log(`${userFullName} was removed from the whitelist.`);
+            }
 
-//             return;
-//         }
+            return;
+        }
 
-//         const didAdd = OptionsController.addToWhitelist(currentMember.user.id);
-//         if (didAdd) {
-//             Logger.log(`${userFullName} was added to the whitelist.`);
-//         }
-//     }
-// }
+        const didAdd = OptionsController.addToWhitelist(currentMember.user.id);
+        if (didAdd) {
+            Logger.log(`${userFullName} was added to the whitelist.`);
+        }
+    }
+}
 
 export default function loader() {
     console.log('feature not ready');
 
-    // if (DEFAULT_CONFIG.USE_DISCORD_BOT) {
-    //     if (!process.env.DISCORD_BOT) {
-    //         Logger.error(
-    //             `.env is missing DISCORD_BOT secret for logging in. Don't forget to add WHITELIST_ROLE as well.`
-    //         );
-    //         return;
-    //     }
+    if (DEFAULT_CONFIG.USE_DISCORD_BOT) {
+        if (!process.env.DISCORD_BOT) {
+            Logger.error(
+                `.env is missing DISCORD_BOT secret for logging in. Don't forget to add WHITELIST_ROLE as well.`
+            );
+            return;
+        }
 
-    //     DiscordController.populateEndpoints();
-    // }
+        DiscordController.populateEndpoints();
+    }
 }
