fc5a463bbab4f42a263c5e2683ae15ad1047ab5e
Stuyk <stuykgaming@gmail.com>
2021-07-18 19:17:15 -0600

    Add Config Check for Colin and fix Typo

--- src/core/server/utility/config.ts
+++ src/core/server/utility/config.ts
@@ -4,8 +4,13 @@ import logger from './athenaLogger';
 export default {
     validate: (config: IConfig): boolean => {
         if (!config.WEBSERVER_IP) {
-            logger.warning(`Please add the field 'IP' to your .env file.`);
+            logger.warning(`Please add the field 'WEBSERVER_IP' to your .env file.`);
             logger.warning(`===> Your IP is your actual server IP. This is used for routing traffic.`);
+            process.exit(0);
+        }
+
+        if (config.WEBSERVER_IP.includes(':')) {
+            logger.warning(`Please remove the port from the 'WEBSERVER_IP' variable in your .env file.`);
             process.exit(0);
         }
 
