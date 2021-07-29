8a3e9015c4d672278b1721cb0b17ea16086362ca
Stuyk <stuykgaming@gmail.com>
2021-07-29 13:31:49 -0600

    Move Interior Info Interface

---
+++ src/core/server/interface/InteriorInfo.ts
@@ -0,0 +1,57 @@
+import * as alt from 'alt-server';
+import { IObject } from '../../shared/interfaces/IObject';
+import { Vector3 } from '../../shared/interfaces/Vector';
+
+export interface InteriorInfo {
+    /**
+     * The ColShape used to enter the interior.
+     * @type {alt.Colshape}
+     * @memberof InteriorInfo
+     */
+    outside: alt.Colshape;
+
+    /**
+     * The position to use to initialize the inside ColShape.
+     * @type {Vector3}
+     * @memberof InteriorInfo
+     */
+    insidePosition: Vector3;
+
+    /**
+     * The position to use to initialize the outside ColShape.
+     * @type {Vector3}
+     * @memberof InteriorInfo
+     */
+    outsidePosition: Vector3;
+
+    /**
+     * The ColShape used to exit the interior.
+     * This ColShape is only created when a player is 'entering' the interior.
+     * @type {alt.Colshape}
+     * @memberof InteriorInfo
+     */
+    inside?: alt.Colshape;
+
+    /**
+     * The IPL to load if necessary when 'entering' the interior.
+     * @type {string}
+     * @memberof InteriorInfo
+     */
+    ipl?: string;
+
+    /**
+     * List of players inside of the interior.
+     * Does not remove players who logout or are no longer valid.
+     * Check for validity yourself if you use this.
+     * @type {Array<alt.Player>}
+     * @memberof InteriorInfo
+     */
+    players: Array<alt.Player>;
+
+    /**
+     * Objects to create / delete in the interior.
+     * @type {Array<IObject>}
+     * @memberof InteriorInfo
+     */
+    objects: Array<IObject>;
+}
--- src/core/server/systems/interior.ts
+++ src/core/server/systems/interior.ts
@@ -4,70 +4,16 @@ import * as alt from 'alt-server';
 import { SYSTEM_EVENTS } from '../../shared/enums/system';
 import { Interior } from '../../shared/interfaces/Interior';
 import { IObject } from '../../shared/interfaces/IObject';
-import { Vector3 } from '../../shared/interfaces/Vector';
 import { DEFAULT_CONFIG } from '../athena/main';
 import { playerFuncs } from '../extensions/Player';
 import { Collections } from '../interface/DatabaseCollections';
+import { InteriorInfo } from '../interface/InteriorInfo';
 import Logger from '../utility/athenaLogger';
 import { getMissingNumber } from '../utility/math';
 import { InteractionController } from './interaction';
 import { MarkerController } from './marker';
 import { ObjectController } from './object';
 import { TextLabelController } from './textlabel';
-
-interface InteriorInfo {
-    /**
-     * The ColShape used to enter the interior.
-     * @type {alt.Colshape}
-     * @memberof InteriorInfo
-     */
-    outside: alt.Colshape;
-
-    /**
-     * The position to use to initialize the inside ColShape.
-     * @type {Vector3}
-     * @memberof InteriorInfo
-     */
-    insidePosition: Vector3;
-
-    /**
-     * The position to use to initialize the outside ColShape.
-     * @type {Vector3}
-     * @memberof InteriorInfo
-     */
-    outsidePosition: Vector3;
-
-    /**
-     * The ColShape used to exit the interior.
-     * This ColShape is only created when a player is 'entering' the interior.
-     * @type {alt.Colshape}
-     * @memberof InteriorInfo
-     */
-    inside?: alt.Colshape;
-
-    /**
-     * The IPL to load if necessary when 'entering' the interior.
-     * @type {string}
-     * @memberof InteriorInfo
-     */
-    ipl?: string;
-
-    /**
-     * List of players inside of the interior.
-     * Does not remove players who logout or are no longer valid.
-     * Check for validity yourself if you use this.
-     * @type {Array<alt.Player>}
-     * @memberof InteriorInfo
-     */
-    players: Array<alt.Player>;
-
-    /**
-     * Objects to create / delete in the interior.
-     * @type {Array<IObject>}
-     * @memberof InteriorInfo
-     */
-    objects: Array<IObject>;
-}
 
 let interiors: Array<Interior> = [];
 let interiorData: { [id: string]: InteriorInfo } = {};
