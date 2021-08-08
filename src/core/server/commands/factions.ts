3642842857bfd60a83ac3ae163c4e5517023ca3a
Stuyk <stuykgaming@gmail.com>
2021-08-08 17:41:20 -0600

    Faction TakeOver Commands, Hand Off Commands, and Create Commands

--- src/core/server/commands/factions.ts
+++ src/core/server/commands/factions.ts
@@ -6,8 +6,79 @@ import { playerFuncs } from '../extensions/Player';
 import ChatController from '../systems/chat';
 import { FactionInternalSystem } from '../systems/factionsInternal';
 
-ChatController.addCommand('createfaction', 'creajfkldsa;fj', PERMISSIONS.ADMIN, handleCommand);
+function handleCreate(player: alt.Player, ...name: string[]): void {
+    if (!player || !player.valid || player.data.faction) {
+        playerFuncs.emit.message(player, `Leave your faction or transfer ownership before creating a new one.`);
+        return;
+    }
 
-function handleCommand(player: alt.Player): void {
-    FactionInternalSystem.create(player, { name: 'Whatever', pos: player.pos, logs: [] });
+    if (!name || name.length <= 0) {
+        playerFuncs.emit.message(player, `Must Specify Faction Name`);
+        return;
+    }
+
+    const actualName = name.join(' ');
+    FactionInternalSystem.create(player, { name: actualName, pos: player.pos, logs: [] });
 }
+
+async function handleTakeover(player: alt.Player, id: string) {
+    if (!id) {
+        return;
+    }
+
+    const faction = FactionInternalSystem.get(id);
+    if (!faction) {
+        playerFuncs.emit.message(player, `Not a Valid Faction`);
+        return;
+    }
+
+    const result = await FactionInternalSystem.takeOverFaction(player, id);
+    if (!result) {
+        playerFuncs.emit.message(player, `Could not take faction.`);
+        return;
+    }
+
+    playerFuncs.emit.message(player, `Took Over Faction: ${faction.name}`);
+}
+
+async function handleHandOff(player: alt.Player) {
+    if (!player || !player.data || !player.data.faction) {
+        return;
+    }
+
+    const faction = FactionInternalSystem.get(player.data.faction);
+    if (!faction) {
+        playerFuncs.emit.message(player, `Not a Valid Faction`);
+        return;
+    }
+
+    const nextInLine = faction.players[1];
+    if (!nextInLine) {
+        playerFuncs.emit.message(player, `Cannot hand off faction to next player.`);
+        return;
+    }
+
+    const result = await FactionInternalSystem.handOffFaction(faction._id.toString(), nextInLine.id, true);
+    if (!result) {
+        playerFuncs.emit.message(player, `Could not hand off faction.`);
+        return;
+    }
+
+    playerFuncs.emit.message(player, `Handed off faction to ${nextInLine.name}`);
+}
+
+ChatController.addCommand(
+    'factiontake',
+    '/factiontake [id] - Take Faction Ownership',
+    PERMISSIONS.ADMIN,
+    handleTakeover
+);
+
+ChatController.addCommand(
+    'factionhandoff',
+    '/factionhandoff - Hand off faction ownership.',
+    PERMISSIONS.ADMIN,
+    handleHandOff
+);
+
+ChatController.addCommand('factioncreate', '/factioncreate [name] - Create a faction', PERMISSIONS.ADMIN, handleCreate);
