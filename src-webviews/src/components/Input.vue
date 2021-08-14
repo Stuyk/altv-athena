91de43e57e88fba53c5d9e6e5164feda57b6a707
Stuyk <stuykgaming@gmail.com>
2021-08-14 12:48:18 -0600

    Input Boxes

---
+++ src-webviews/src/components/Input.vue
@@ -0,0 +1,138 @@
+<template>
+    <div>
+        <div :class="inputClass" class="input">
+            <div class="split">
+                <div :class="labelClass" class="label overline" v-if="label">{{ this.label }}</div>
+                <Icon v-if="icon" class="grey--text pr-4" :size="24" :icon="icon" />
+            </div>
+            <input :class="textboxClass" class="textbox pa-2" v-model="text" :type="numberOnly ? 'number' : 'text'" :placeholder="placeholder" @keyup="handleInput"/>
+            <div v-if="errorMessage && stack" class="red--text text--lighten-2 text--subtitle-2 pt-2">{{ errorMessage }}</div>
+        </div>
+        <div v-if="errorMessage && !stack" class="red--text text--lighten-2 text--subtitle-2 pt-2">{{ errorMessage }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Icon from './Icon.vue';
+
+const ComponentName = 'Input';
+export default defineComponent({
+  name: ComponentName,
+  components: {
+      Icon
+  },
+  data() {
+      return {
+          text: '',
+          errorMessage: null
+      }
+  },
+  props: {
+      placeholder: {
+          type: String,
+          required: false
+      },
+      onInput: {
+          type: Function,
+          required: true
+      },
+      label: {
+          type: String,
+          required: false
+      },
+      stack: {
+          type: Boolean,
+          required: false
+      },
+      numberOnly: {
+          type: Boolean,
+          required: true
+      },
+      icon: {
+          type: String,
+          required: false
+      },
+      rules: {
+          type: Array,
+          required: false
+      },
+      validateCallback: {
+          type: Function,
+          required: false
+      },
+      value: String
+  },
+  methods: {
+      handleInput() {
+          if (!this.onInput) {
+              return;
+          }
+
+          // Validate Rules
+          if (this.rules && Array.isArray(this.rules)) {
+              this.errorMessage = null;
+              let didPassAll = true;
+
+              for(let i = 0; i < this.rules.length; i++) {
+                  const ruleToTest = this.rules[i];
+                  if (typeof ruleToTest !== 'function') {
+                      continue;
+                  }
+
+                  // If there is something returned...
+                  // Then give this input an error message and break.
+                  const result = ruleToTest(this.text);
+                  if (result) {
+                      this.errorMessage = result;
+                      didPassAll = false;
+                      break;
+                  }
+              }
+
+              if (this.validateCallback && typeof this.validateCallback === 'function') {
+                  this.validateCallback(didPassAll);
+              }
+          }
+
+          this.onInput(this.text)
+      }
+  },
+  computed: {
+      inputClass() {
+          const classes = {}
+
+          if (this.stack) {
+              classes['input-stack'] = true;
+          } else {
+              classes['input-full-width'] = true;
+          }
+
+          return classes;
+      },
+      labelClass() {
+          const classes = {}
+
+          if (!this.stack) {
+            classes['pr-2'] = true;
+          }
+
+          return classes;
+      },
+      textboxClass() {
+           const classes = {}
+
+          if (!this.stack) {
+              classes['textbox-full-width'] = true;
+          }
+
+          return classes;
+      }
+  },
+  mounted() {
+      if (this.value) {
+          this.text = this.value;
+      }
+  }
+})
+</script>
--- src-webviews/src/pages/designs/Designs.vue
+++ src-webviews/src/pages/designs/Designs.vue
@@ -39,32 +39,74 @@ Helpful for seeing what default components look like.
                 <Icon class="grey--text" :size="32" icon="icon-server" />
                 <Icon class="grey--text" :size="64" icon="icon-user" />
             </div>
+            <h2 class="grey--text text--lighten-2">Input Boxes</h2>
+            <h4 class="grey--text text--lighten-2">Basic</h4>
+            <Input  
+                :stack="true" 
+                :onInput="onInputChange" 
+                placeholder="Input First Name..."
+            />
+            <h4 class="grey--text text--lighten-2">Basic Unstacked</h4>
+            <Input 
+                label="First Name" 
+                :stack="false" 
+                :onInput="onInputChange" 
+                placeholder="Hello World" 
+            />
+            <h4 class="grey--text text--lighten-2">Advanced with Rules</h4>
+            <Input 
+                label="Age" 
+                :numberOnly="true" 
+                :stack="false" 
+                :onInput="ageInputChange"
+                :validateCallback="ageValidCallback"
+                :value="5"
+                :rules="[
+                    (text) => { return parseInt(text) >= 18 ? null : 'Age must be at least 18' },
+                    (text) => { return parseInt(text) <= 99 ? null : 'Age cannot exceed 99' },
+                ]"
+                icon="icon-question"
+                placeholder="25..." 
+            />
         </div>
     </div>
-
-    
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Input from '../../components/Input.vue';
 import Button from '../../components/Button.vue';
 import Icon from '../../components/Icon.vue';
 
 const ComponentName = 'Designs';
 export default defineComponent({
   name: ComponentName,
+  data() {
+      return {
+          ageValid: false
+      }
+  },
   components: {
       Button,
-      Icon
+      Icon,
+      Input
   },
   methods: {
       clickMe(e) {
           console.log(e);
+      },
+      onInputChange(text: string) {
+          console.log(text);
+      },
+      ageValidCallback(valid: boolean) {
+          this.ageValid = valid;
+          console.log(`Is Age Valid: ${this.ageValid}`);
+      },
+      ageInputChange(text: string) {
+          console.log(`Age Input as: ${text}`);
       }
   },
-  data() {
-      return {}
-  },
+  
 })
 </script>
 
