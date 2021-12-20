65f44fd9f41af6c2c98658b62ee31c0be982d6f1
stuyk <stuykgaming@gmail.com>
2021-12-20 09:28:57 -0700

    More Interior Work

--- src/core/plugins/core-interior/src/system.ts
+++ src/core/plugins/core-interior/src/system.ts
@@ -15,6 +15,9 @@ import { INTERIOR_SYSTEM } from '../../../shared-plugins/core-interiors/flags';
 import { INTERIOR_RULES } from '../../../shared-plugins/core-interiors/enums';
 import SystemRules from '../../../server/systems/rules';
 import { IResponse } from '../../../shared/interfaces/IResponse';
+import { SYSTEM_EVENTS } from '../../../shared/enums/System';
+import { ServerObjectController } from '../../../server/streamers/object';
+import { ServerTextLabelController } from '../../../server/streamers/textlabel';
 
 /**
  * Interiors should work in the following way.
@@ -22,7 +25,10 @@ import { IResponse } from '../../../shared/interfaces/IResponse';
  * However, if one is not provided we simply create a uid for the interior.
  * Everything should work around the uid for adding, looking up, etc.
  */
+const ONE_BILLION = 1000000000;
+const DOOR_CHECK_DIST = 3;
 const LOST_PLAYER_POS = { x: -867.1437377929688, y: -172.6201934814453, z: 37.799232482910156 };
+const NEW_LINE = `~n~`;
 const INTERIOR_COLLECTION = 'interiors';
 const interiors: Map<string, InteriorInternal> = new Map();
 
@@ -74,8 +80,8 @@ class InternalSystem {
             callback: InteriorView.showMenu,
         });
 
+        InternalSystem.refreshInteriorText(interiorInfo);
         interiors.set(interior.uid, interiorInfo);
-        // InteriorSystem.refreshHouseText(interior);
     }
 
     /**
@@ -105,14 +111,11 @@ class InternalSystem {
      * @param {alt.Player} player
      * @memberof InternalSystem
      */
-    static removePlayer(player: alt.Player) {
+    static removePlayer(player: alt.Player, interior: InteriorInternal = null) {
         const id = player.id;
+        const data = player.data;
 
-        if (!player.interior) {
-            return;
-        }
-
-        const interior = interiors.get(player.interior);
+        interior = data.interior ? interiors.get(data.interior) : interior;
         if (!interior) {
             return;
         }
@@ -127,6 +130,78 @@ class InternalSystem {
         }
 
         interior.players.splice(index, 1);
+    }
+
+    /**
+     * Refreshes an interior and for anyone inside of it.
+     * @static
+     * @param {string} id
+     * @memberof InteriorSystem
+     */
+    static async refreshInteriorForPlayer(player: alt.Player) {
+        if (!player || !player.valid || !player.data || player.data.interior === null) {
+            return;
+        }
+
+        const interior = interiors.get(player.data.interior);
+        if (!interior) {
+            return;
+        }
+
+        const objects = interior.objects;
+
+        for (let i = 0; i < objects.length; i++) {
+            ServerObjectController.removeFromPlayer(player, objects[i].uid, true);
+        }
+
+        // Reset Objects and Re-Add Them
+        if (player.dimension === 0) {
+            return;
+        }
+
+        for (let i = 0; i < objects.length; i++) {
+            objects[i].isInterior = true;
+            ServerObjectController.addToPlayer(player, objects[i]);
+        }
+    }
+
+    /**
+     * Generates text to display outside based on interior properties.
+     * Refreshes label if present.
+     * @private
+     * @static
+     * @param {Interior} interior
+     * @memberof InternalSystem
+     */
+    private static refreshInteriorText(interior: InteriorInternal): void {
+        // Begin Outside Text Label Information
+        let outsideName = '';
+
+        outsideName += `${interior.name}`;
+
+        if (!interior.isUnlocked) {
+            outsideName += NEW_LINE;
+            outsideName += `~r~Locked~w~`;
+        }
+
+        if (interior.price >= 1) {
+            outsideName += NEW_LINE;
+            outsideName += `~g~Price: $${interior.price}~w~`;
+        }
+
+        const aboveGroundOutside = {
+            x: interior.outside.x,
+            y: interior.outside.y,
+            z: interior.outside.z + 0.75,
+        };
+
+        ServerTextLabelController.remove(`${interior.uid}-outside`);
+        ServerTextLabelController.append({
+            uid: `${interior.uid}-outside`,
+            pos: aboveGroundOutside,
+            data: outsideName,
+            maxDistance: 10,
+        });
     }
 }
 
@@ -204,8 +279,27 @@ export class InteriorSystem {
             return false;
         }
 
-        // Delete colshapes, objects, etc.
         // Remove players inside interior if inside.
+        if (interior.players.length >= 1) {
+            const foundPlayers = alt.Player.all.filter((p) => interior.players.findIndex((ip) => ip === p.id) >= 0);
+            for (let i = 0; i < foundPlayers.length; i++) {
+                const target = foundPlayers[i];
+                if (!target || !target.valid) {
+                    continue;
+                }
+
+                InteriorSystem.movePlayerOut(target, true);
+            }
+        }
+
+        // Delete colshapes, objects, etc.
+        try {
+            ServerMarkerController.remove(`${interior.uid}-outside`);
+            ServerMarkerController.remove(`${interior.uid}-inside`);
+
+            interior.outsideShape.destroy();
+            interior.insideShape.destroy();
+        } catch (err) {}
 
         await Database.deleteById(interior._id.toString(), INTERIOR_COLLECTION);
         return interiors.delete(uid);
@@ -226,7 +320,7 @@ export class InteriorSystem {
 
         if (!skipDistanceCheck) {
             const dist = distance(player.pos, interior.outside);
-            if (dist >= 3) {
+            if (dist > DOOR_CHECK_DIST) {
                 playerFuncs.emit.notification(player, 'LOCALE_TOO_FAR_FROM_DOOR');
                 return false;
             }
@@ -268,8 +362,7 @@ export class InteriorSystem {
         }
 
         if (interior.ipl) {
-            // DO IPL THINGS WOO
-            // alt.emitClient(player, SYSTEM_EVENTS.IPL_LOAD, interior.ipl);
+            alt.emitClient(player, SYSTEM_EVENTS.IPL_LOAD, interior.ipl);
         }
 
         playerFuncs.safe.setDimension(player, interior.dimension);
@@ -285,6 +378,12 @@ export class InteriorSystem {
                 playerFuncs.set.frozen(player, false);
             }, 1000);
         }
+
+        player.data.interior = interior.uid;
+        playerFuncs.save.field(player, 'interior', player.data.interior);
+        InternalSystem.refreshInteriorForPlayer(player);
+
+        return true;
     }
 
     /**
@@ -295,8 +394,8 @@ export class InteriorSystem {
      * @param {string} uid
      * @memberof InteriorSystem
      */
-    static async movePlayerOut(player: alt.Player): Promise<boolean> {
-        const interior = await InteriorSystem.get(player.interior);
+    static async movePlayerOut(player: alt.Player, skipDistanceCheck = false): Promise<boolean> {
+        const interior = await InteriorSystem.get(player.data.interior);
 
         if (!interior) {
             playerFuncs.safe.setDimension(player, 0);
@@ -305,11 +404,81 @@ export class InteriorSystem {
         }
 
         if (interior.ipl) {
-            // Unload IPL
-            // alt.emitClient(player, SYSTEM_EVENTS.IPL_UNLOAD, interior.ipl);
+            alt.emitClient(player, SYSTEM_EVENTS.IPL_UNLOAD, interior.ipl);
         }
 
-        // We should send the player somewhere if they don't have interior set.
+        const dist = distance(player.pos, interior.inside);
+        if (dist > DOOR_CHECK_DIST) {
+            playerFuncs.emit.notification(player, 'LOCALE_TOO_FAR_FROM_EXIT');
+            return false;
+        }
+
+        if (!SystemRules.check(INTERIOR_RULES.EXIT, rules, player, interior)) {
+            return false;
+        }
+
+        InternalSystem.removePlayer(player, interior);
+        InternalSystem.refreshInteriorForPlayer(player);
+
+        playerFuncs.set.frozen(player, true);
+        playerFuncs.safe.setDimension(player, 0);
+        playerFuncs.safe.setPosition(player, interior.outside.x, interior.outside.y, interior.outside.z);
+        player.data.interior = null;
+        playerFuncs.save.field(player, 'interior', player.data.interior);
+        alt.setTimeout(() => {
+            if (!player || !player.valid) {
+                return;
+            }
+
+            playerFuncs.set.frozen(player, false);
+        }, 1000);
+
+        return true;
+    }
+
+    /**
+     * Used for checking if a player is an owner of an interior.
+     * @static
+     * @param {alt.Player} player
+     * @param {Interior} interior
+     * @return {boolean}
+     * @memberof InteriorSystem
+     */
+    static isOwner(player: alt.Player, interior: Interior): boolean {
+        if (!player || !player.valid || player.data.isDead || !interior) {
+            return false;
+        }
+
+        if (player.data._id.toString() === interior.owner) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Used for locking / unlocking an interior.
+     * @static
+     * @param {alt.Player} player
+     * @param {Interior} interior
+     * @return {boolean}
+     * @memberof InteriorSystem
+     */
+    static hasAccess(player: alt.Player, interior: Interior): boolean {
+        if (!player || !player.valid || player.data.isDead || !interior) {
+            return false;
+        }
+
+        if (interior.keys && interior.keys.findIndex((id) => id === player.data._id.toString()) >= 0) {
+            return true;
+        }
+
+        const hasFactionSupport = interior.factions && player.data.faction;
+        if (hasFactionSupport && interior.factions.findIndex((id) => id === player.data.faction) >= 0) {
+            return true;
+        }
+
+        return false;
     }
 }
 
