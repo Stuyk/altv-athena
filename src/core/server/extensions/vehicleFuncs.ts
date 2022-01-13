3474ce09b061cea06ee16ef25ecaacc0968dbb2e
franko <49619925+FrankoFM@users.noreply.github.com>
2022-01-13 19:04:24 +0100

    Added some config options (#113)
    * Added VEHICLE_DISPLAY_LOCK_STATUS to config
    * Added *_INTERACTION_INFO to config

--- src/core/server/extensions/vehicleFuncs.ts
+++ src/core/server/extensions/vehicleFuncs.ts
@@ -278,7 +278,8 @@ export default class VehicleFuncs {
         vehicle.passengers = [];
         vehicle.customPrimaryColor = new alt.RGBA(255, 255, 255, 255);
         vehicle.customSecondaryColor = new alt.RGBA(255, 255, 255, 255);
-        vehicle.setStreamSyncedMeta(VEHICLE_STATE.LOCKSYMBOL, true);
+        vehicle.setStreamSyncedMeta(VEHICLE_STATE.LOCKSYMBOL, DEFAULT_CONFIG.VEHICLE_DISPLAY_LOCK_STATUS);
+        vehicle.setStreamSyncedMeta(VEHICLE_STATE.LOCK_INTERACTION_INFO, DEFAULT_CONFIG.VEHICLE_DISPLAY_LOCK_INTERACTION_INFO);
 
         // Setup Default Document Values
         if (document.fuel === null || document.fuel === undefined) {
@@ -400,7 +401,8 @@ export default class VehicleFuncs {
         }
 
         vehicle.setStreamSyncedMeta(VEHICLE_STATE.OWNER, vehicle.player_id);
-        vehicle.setStreamSyncedMeta(VEHICLE_STATE.LOCKSYMBOL, true);
+        vehicle.setStreamSyncedMeta(VEHICLE_STATE.LOCKSYMBOL, DEFAULT_CONFIG.VEHICLE_DISPLAY_LOCK_STATUS);
+        vehicle.setStreamSyncedMeta(VEHICLE_STATE.LOCK_INTERACTION_INFO, DEFAULT_CONFIG.VEHICLE_DISPLAY_LOCK_INTERACTION_INFO);
         return vehicle;
     }
 
