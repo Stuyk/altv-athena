2aa8e5eeae4002b3655a6c76b2546715302eae86
xLuxy <67131061+xLuxy@users.noreply.github.com>
2022-04-13 14:28:57 +0200

    Add Water and Cola item

--- src/core/plugins/core-items/server/src/items.ts
+++ src/core/plugins/core-items/server/src/items.ts
@@ -66,6 +66,48 @@ export const items: Array<Item> = [
         version: 1
     },
     {
+        name: `Water`,
+        description: `A refreshing glass of water.`,
+        dbName: 'water',
+        icon: 'water',
+        slot: 0,
+        maxStack: 3,
+        quantity: 1,
+        behavior:
+            ITEM_TYPE.CAN_DROP |
+            ITEM_TYPE.CAN_TRADE |
+            ITEM_TYPE.CAN_STACK |
+            ITEM_TYPE.IS_TOOLBAR |
+            ITEM_TYPE.CONSUMABLE,
+        data: {
+            event: EFFECTS.EFFECT_WATER,
+            amount: 5,
+            sound: 'item_drink',
+        },
+        version: 1
+    },
+    {
+        name: `Cola`,
+        description: `A refreshing glass of cola.`,
+        dbName: 'cola',
+        icon: 'cola',
+        slot: 0,
+        maxStack: 3,
+        quantity: 1,
+        behavior:
+            ITEM_TYPE.CAN_DROP |
+            ITEM_TYPE.CAN_TRADE |
+            ITEM_TYPE.CAN_STACK |
+            ITEM_TYPE.IS_TOOLBAR |
+            ITEM_TYPE.CONSUMABLE,
+        data: {
+            event: EFFECTS.EFFECT_WATER,
+            amount: 25,
+            sound: 'item_drink',
+        },
+        version: 1
+    },
+    {
         name: `Repair Kit`,
         description: `A toolkit to repair a vehicle.`,
         dbName: 'repair-kit',
