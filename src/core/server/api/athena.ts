d859f1c68325ba4bad1f3439b0f3f8b5986059d0
stuyk <stuykgaming@gmail.com>
2022-04-14 12:16:00 -0600

    Add Athena API

--- src/core/server/api/athena.ts
+++ src/core/server/api/athena.ts
@@ -2,12 +2,28 @@ import { VehicleController } from '../../client/systems/vehicle';
 import { playerFuncs } from '../extensions/extPlayer';
 import VehicleFuncs from '../extensions/vehicleFuncs';
 import { Controllers } from '../streamers/controllers';
+import { AgendaSystem } from '../systems/agenda';
+import { ItemFactory } from '../systems/item';
+import ChatController from '../systems/chat';
+import { ItemEffects } from '../systems/itemEffects';
+import { PluginSystem } from '../systems/plugins';
+import { StorageSystem } from '../systems/storage';
+import { World } from '../systems/world';
 
 export const Athena = {
+    controllers: Controllers,
     player: playerFuncs,
+    systems: {
+        chat: ChatController,
+        agenda: AgendaSystem,
+        items: ItemFactory,
+        effects: ItemEffects,
+        plugins: PluginSystem,
+        storage: StorageSystem,
+        world: World,
+    },
     vehicle: {
         controller: VehicleController,
         funcs: VehicleFuncs,
     },
-    controllers: Controllers,
 };
