6a9e200a92205397c5c40d5b2a9b30394e71f1dc
Marcel N <67131061+xLuxy@users.noreply.github.com>
2022-04-18 13:24:39 +0200

    Enable decorator support and add commands decorator (#136)
    * Enable decorator support and add commands decorator
    
    * Move command decorator into server

---
+++ src/core/server/decorators/commands.ts
@@ -0,0 +1,8 @@
+import { Athena } from '../../server/api/athena';
+import { PERMISSIONS } from '../../shared/flags/permissionFlags';
+
+export function command(commandName: string, description: string, permissions: PERMISSIONS) {
+    return (_target: Function, _propertyKey: string, descriptor: PropertyDescriptor) => {
+        Athena.controllers.chat.addCommand(commandName, description, permissions, descriptor.value);
+    };
+}
