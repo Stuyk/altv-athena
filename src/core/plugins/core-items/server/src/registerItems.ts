394af1f9529e9ead410567617da22ed87f5d5af3
Der Lord! <82890183+Booster1212@users.noreply.github.com>
2022-04-19 19:05:46 +0200

    Better Update Handling, sorted lists. (#138)
    * Better Update Handling, sorted lists.
    
    * Switch item versions back to 1
    
    * Resolve Review issues
    
    * Add spaces and fix imports
    
    * Switch continue statement

--- src/core/plugins/core-items/server/src/registerItems.ts
+++ src/core/plugins/core-items/server/src/registerItems.ts
@@ -1,13 +1,12 @@
 import { ItemFactory } from '../../../../server/systems/item';
 import { ITEM_TYPE } from '../../../../shared/enums/itemTypes';
 import { getWeaponList } from '../../../../shared/information/weaponList';
-import { items } from './items';
+import items from './items';
 
 export class RegisterItems {
     static async init() {
-        const itemsToAdd = [...items];
-        for (let i = 0; i < itemsToAdd.length; i++) {
-            ItemFactory.add(itemsToAdd[i]);
+        for (let i = 0; i < items.length; i++) {
+            ItemFactory.add(items[i]);
         }
 
         const weaponList = getWeaponList();
