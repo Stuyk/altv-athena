3be0f9811522169e31ecb0e54cad29898d3c163b
Stuyk <stuykgaming@gmail.com>
2022-04-20 08:00:50 -0600

    Update src/core/server/decorators/interaction.ts
    Co-authored-by: Narcis B. <leonardssh22@gmail.com>

--- src/core/server/decorators/interaction.ts
+++ src/core/server/decorators/interaction.ts
@@ -26,19 +26,18 @@ let data: Interaction = {
  * ```
  * class ExampleDecoratorInteraction {
  *      @interaction({ 
- *      position: { x: 0, y: 0, z: 71 }, 
- *      description: 'Hello World!', 
- *      range: 25, 
- *      dimension: 0, 
- *      uid: 'UUID', 
- *      debug: false, 
- *      isPlayerOnly: false, 
- *      isVehicleOnly: false 
- *  })
- * 
- *  static doSomething() {
- *      alt.logError("Hello World");
- *  }
+ *          position: { x: 0, y: 0, z: 71 }, 
+ *          description: 'Hello World!', 
+ *          range: 25, 
+ *          dimension: 0, 
+ *          uid: 'UUID', 
+ *          debug: false, 
+ *          isPlayerOnly: false, 
+ *          isVehicleOnly: false 
+ *      })
+ *      static doSomething() {
+ *          alt.logError("Hello World");
+ *      }
  *}
  * ```
  * @param {IVector3} data.position - The position of the interaction.
