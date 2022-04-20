2d772f15a2886f8aed38d07e17776d7d27a423cf
Der Lord! <82890183+Booster1212@users.noreply.github.com>
2022-04-20 12:27:55 +0200

    Add config value to skip character-selector.

--- src/core/plugins/core-character-select/server/src/view.ts
+++ src/core/plugins/core-character-select/server/src/view.ts
@@ -110,7 +110,7 @@ class InternalFunctions {
             playerCharacters[i]._id = playerCharacters[i]._id.toString();
         }
 
-        if (CHARACTER_SELECT_CONFIG.MAX_CHARACTERS <= 1) {
+        if (CHARACTER_SELECT_CONFIG.MAX_CHARACTERS <= 1 && CHARACTER_SELECT_CONFIG.SKIP_SELECTOR) {
             if (CharacterList[player.id]) {
                 delete CharacterList[player.id];
             }
