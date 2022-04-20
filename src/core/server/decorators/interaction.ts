90fb0c1c74bada140d13c43bdd52741de73e291c
Der Lord! <82890183+Booster1212@users.noreply.github.com>
2022-04-20 14:22:33 +0200

    Add example of how to use it.

--- src/core/server/decorators/interaction.ts
+++ src/core/server/decorators/interaction.ts
@@ -1,15 +1,32 @@
 import alt, { IVector3 } from 'alt-server';
 import { Athena } from '../api/athena';
 
+/**
+ * @param {IVector3} pos - The position of the interaction.
+ * @param {string} description - The description of the interaction.
+ * @param {number} range - The range of the interaction.
+ * @param {number} dim - The dimension to search in.
+ * @param {string} uid - The uid of the interaction.
+ * Decorator for Athena's Interaction Controller
+ * ```
+ * class ExampleDecorators {
+ *   @interaction({ x: 0, y: 0, z: 0 }, 'Hello World!', 25, 0, 'UUID')
+ *   static exampleInteraction() {
+ *      alt.log("Interaction controller got triggerd by player through decorator!");
+ *   }
+ * }
+ * ```
+ */
+
 export function interaction(
     pos: IVector3,
     description: string,
     range: number,
     dim: number,
+    uid: string,
     debug?: boolean,
     playerOnly?: boolean,
     vehicleOnly?: boolean,
-    uid?: string,
 ) {
     return (_target: Function, _propertyName: string, descriptor: PropertyDescriptor) => {
         const callback: (player: alt.Player, ...args: any[]) => void = descriptor.value;
