28fa469e9ca6103b2ebb02652ad983c32a388600
stuyk <stuykgaming@gmail.com>
2022-04-24 07:15:37 -0600

    add 3399 to killport script, add faction veh prep

--- scripts/killport/index.js
+++ scripts/killport/index.js
@@ -1,5 +1,5 @@
 import fkill from 'fkill';
-const ports = [7788, 'altv-server', 'altv-server.exe'];
+const ports = [7788, 'altv-server', 'altv-server.exe', 3399];
 
 for (let i = 0; i < ports.length; i++) {
     try {
