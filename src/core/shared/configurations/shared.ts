0fd7415c513378631bccb0b6feb7c99d0aadf1c9
Shirobaka <kegy5@live.de>
2022-04-27 03:49:40 +0200

    Removed FORCE_SCREEN_SHAKE
    Removed FORCE_SCREEN_SHAKE since its not used anymore

--- src/core/shared/configurations/shared.ts
+++ src/core/shared/configurations/shared.ts
@@ -1,8 +1,6 @@
 export const SHARED_CONFIG = {
     // Default Respawn Time for Player
     RESPAWN_TIME: 25000,
-    // PLAYERS
-    SHOULD_SCREEN_SHAKE: true, // Used to shake the player's screen. Makes most aimbots a hell of a lot worse.
     // FOOD & WATER
     FOOD_FATIGUE: 10, // If food drops below this number the player stops sprinting.
     WATER_FATIGUE: 8, // If water drops below this number the player stops sprinting.
