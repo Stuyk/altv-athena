6f027a05185a58cb8bc3ba1a2c95e42b8e537ac7
Rafael Martins <rafaelmartinsrm@gmail.com>
2022-04-29 19:45:31 -0300

    Adds VehicleMenuInjections and creates the VehicleMenu class

--- src/core/client/menus/vehicle.ts
+++ src/core/client/menus/vehicle.ts
@@ -9,56 +9,84 @@ import { IClientWheelItem, WheelMenu } from '../utility/wheelMenu';
 import { SYSTEM_EVENTS } from '../../shared/enums/system';
 import { VEHICLE_EVENTS } from '../../shared/enums/vehicle';
 
-function openMenu() {
-    if (isAnyMenuOpen()) {
-        return;
+const VehicleMenuInjections: Array<
+    (player: alt.Player, vehicle: alt.Vehicle, options?: Array<IClientWheelItem>) => Array<IClientWheelItem> | void
+> = [];
+
+export default class VehicleMenu {
+    static addVehicleMenuInjections(
+        callback: (
+            player: alt.Player,
+            vehicle: alt.Vehicle,
+            options?: Array<IClientWheelItem>,
+        ) => Array<IClientWheelItem> | void,
+    ) {
+        VehicleMenuInjections.push(callback);
     }
 
-    const vehicle = getClosestVectorByPos<alt.Vehicle>(alt.Player.local.pos, alt.Vehicle.all, 'pos');
-    if (!vehicle || !vehicle.valid) {
-        return;
-    }
-
-    const dist = distance(alt.Player.local.pos, vehicle.pos);
-    if (dist >= 4) {
-        return;
-    }
-
-    const options: Array<IClientWheelItem> = [];
-
-    if (!alt.Player.local.vehicle) {
-        const isDestroyed = native.getVehicleEngineHealth(vehicle.scriptID) <= 0;
-
-        // Not Pushing & Vehicle is Currently Unlocked
-        if (!PushVehicle.isPushing() && native.getVehicleDoorLockStatus(vehicle.scriptID) !== 2 && !isDestroyed) {
-            options.push({
-                name: 'Push',
-                callback: PushVehicle.start,
-                data: [vehicle],
-            });
-
-            options.push({
-                name: 'Open Storage',
-                callback: () => {
-                    alt.emitServer(VEHICLE_EVENTS.OPEN_STORAGE, vehicle);
-                },
-            });
-        } else {
-            options.push({
-                name: 'Stop Push',
-                callback: PushVehicle.clear
-            }); 
+    static openMenu() {
+        if (isAnyMenuOpen()) {
+            return;
         }
+
+        const vehicle = getClosestVectorByPos<alt.Vehicle>(alt.Player.local.pos, alt.Vehicle.all, 'pos');
+        if (!vehicle || !vehicle.valid) {
+            return;
+        }
+
+        const dist = distance(alt.Player.local.pos, vehicle.pos);
+        if (dist >= 4) {
+            return;
+        }
+
+        let options: Array<IClientWheelItem> = [];
+
+        if (!alt.Player.local.vehicle) {
+            const isDestroyed = native.getVehicleEngineHealth(vehicle.scriptID) <= 0;
+
+            // Not Pushing & Vehicle is Currently Unlocked
+            if (!PushVehicle.isPushing() && native.getVehicleDoorLockStatus(vehicle.scriptID) !== 2 && !isDestroyed) {
+                options.push({
+                    name: 'Push',
+                    callback: PushVehicle.start,
+                    data: [vehicle],
+                });
+
+                options.push({
+                    name: 'Open Storage',
+                    callback: () => {
+                        alt.emitServer(VEHICLE_EVENTS.OPEN_STORAGE, vehicle);
+                    },
+                });
+            } else {
+                options.push({
+                    name: 'Stop Push',
+                    callback: PushVehicle.clear,
+                });
+            }
+
+            for (const callback of VehicleMenuInjections) {
+                try {
+                    const tempOptions = callback(alt.Player.local, vehicle, options);
+                    if (tempOptions) {
+                        options = tempOptions;
+                    }
+                } catch (err) {
+                    console.warn(`Got Vehicle Menu Injection Error for Player: ${err}`);
+                    continue;
+                }
+            }
+        }
+
+        WheelMenu.create('Vehicle Options', options);
     }
 
-    WheelMenu.create('Vehicle Options', options);
+    static init() {
+        KeybindController.registerKeybind({
+            key: KEY_BINDS.VEHICLE_OPTIONS,
+            singlePress: VehicleMenu.openMenu,
+        });
+    }
 }
 
-function init() {
-    KeybindController.registerKeybind({
-        key: KEY_BINDS.VEHICLE_OPTIONS,
-        singlePress: openMenu,
-    });
-}
-
-alt.onServer(SYSTEM_EVENTS.TICKS_START, init);
+alt.onServer(SYSTEM_EVENTS.TICKS_START, VehicleMenu.init);
