d80209ba6f6073b743a1e7e9f819081660b1a75e
stuyk <stuykgaming@gmail.com>
2022-06-15 11:59:06 -0600

    Expiremntal Runtime Scripts

--- scripts/runtime/index.js
+++ scripts/runtime/index.js
@@ -1,11 +1,17 @@
 import { ChildProcess, spawn } from 'child_process';
+import fkill from 'fkill';
+import glob from 'glob';
+import fs from 'fs';
+import crypto from 'crypto';
 
+const ports = [7788, 'altv-server', 'altv-server.exe', 3399];
 const node = 'node';
 const npx = process.platform === 'win32' ? 'npx.cmd' : 'npx';
 const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
 const serverBinary = process.platform === 'win32' ? 'altv-server.exe' : 'altv-server';
 
 const passedArguments = process.argv.slice(2);
+const fileNameHashes = {};
 
 /**
  * This is the alt:V Server Process
@@ -16,6 +22,34 @@ let lastServerProcess;
  * This is the Streamer Server Process
  * @type {ChildProcess} */
 let lastStreamerProcess;
+
+/**
+ * This is the WebView Dev Server Process
+ * Runs on localhost:3000;
+ * @type {ChildProcess} */
+let lastViteServer;
+
+/**
+ * Previous files used in runtime.
+ * @type {Array<string>}
+ * */
+let previousGlobFiles = [];
+
+let fileWatchTimeout = Date.now() + 1000;
+
+/**
+ * This function waits for a process to be fully killed first.
+ * @param {ChildProcess} process
+ */
+async function killChildProcess(process) {
+    while (process.killed === false) {
+        process.kill();
+
+        await new Promise((resolve) => {
+            setTimeout(resolve, 1000);
+        });
+    }
+}
 
 async function runFile(processName, ...args) {
     const spawnedProcess = spawn(processName, [...args], { stdio: 'inherit' });
@@ -49,24 +83,121 @@ async function handleConfiguration() {
     await runFile(npx, 'altv-config', './scripts/buildresource/resource.json', './resources/core/resource.cfg');
 }
 
-function handleStreamerProcess() {
-    console.log(`Starting Streamer Process`);
+async function handleViteDevServer() {
+    if (lastViteServer && !lastViteServer.killed) {
+        lastViteServer.kill();
+    }
+
+    console.log(`===> Starting WebView Process`);
+    await runFile(node, './scripts/plugins/webview.js');
+    lastViteServer = spawn(
+        npx,
+        ['vite', './src-webviews', '--clearScreen=false', '--debug=true'],
+        { stdio: 'inherit' },
+    );
+
+    lastViteServer.once('close', (code) => {
+        console.log(`Vite process exited with code ${code}`);
+    });
+
+    return await new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    });
+}
+
+function handleStreamerProcess(shouldAutoRestart = false) {
+    if (lastStreamerProcess && !lastStreamerProcess.killed) {
+        lastStreamerProcess.kill();
+    }
+
+    console.log(`===> Starting Streamer Process`);
     lastStreamerProcess = spawn(node, ['./scripts/streamer/dist/index.js'], { stdio: 'inherit' });
 
     lastStreamerProcess.once('close', (code) => {
         console.log(`Streamer process exited with code ${code}`);
-        handleStreamerProcess();
+        if (shouldAutoRestart) {
+            handleStreamerProcess(shouldAutoRestart)
+        }
     });
 }
 
-function handleServerProcess() {
-    console.log(`Starting Server Process`);
+function handleServerProcess(shouldAutoRestart = false) {
+    if (lastServerProcess && !lastServerProcess.killed) {
+        lastServerProcess.kill();
+    }
+
+    console.log(`===> Starting Server Process`);
     lastServerProcess = spawn(serverBinary, { stdio: 'inherit' });
 
     lastServerProcess.once('close', (code) => {
         console.log(`Server process exited with code ${code}`);
-        runServer();
+        if (shouldAutoRestart) {
+            handleServerProcess(shouldAutoRestart);
+        }
     });
+}
+
+async function refreshFileWatching() {
+    // unwatch all old files
+    for (const filePath in previousGlobFiles) {
+        fs.unwatchFile(filePath);
+    }
+
+    // grab all new files
+    const files = glob.sync('./src/**/*.ts');
+
+    // ignore `/athena/server` && `/athena/client` directories 
+    previousGlobFiles = files.filter(fileName => {
+        if (fileName.includes('\/athena\/server\/')) {
+            return false;
+        }
+
+        if (fileName.includes('\/athena\/client\/')) {
+            return false;
+        }
+
+        return true;
+    });
+
+    for (const filePath of previousGlobFiles) {
+        fileNameHashes[filePath] = crypto.createHash('md5').update(fs.readFileSync(filePath)).digest('hex');
+        fs.watch(filePath, (event) => {
+            if (event === 'rename' || event === 'change') {
+                if (Date.now() < fileWatchTimeout) {
+                    return;
+                }
+
+                fileWatchTimeout = Date.now() + 2500;
+
+                const current = crypto.createHash('md5').update(fs.readFileSync(filePath)).digest('hex');
+                if (fileNameHashes[filePath] === current) {
+                    return;
+                }
+
+                devMode(false);
+                console.log(`${filePath} has changed, restarting server`);
+            }
+        });
+    }
+}
+
+async function devMode(firstRun = false) {
+    if (firstRun) {
+        await handleViteDevServer();
+        await refreshFileWatching();
+        return;
+    }
+
+    await killChildProcess(lastStreamerProcess);
+    await killChildProcess(lastServerProcess);
+
+    await runFile(node, './scripts/compiler/core');
+    await runFile(node, './scripts/plugins/core');
+    await runFile(node, './scripts/plugins/webview');
+    await runFile(node, './scripts/plugins/update-dependencies');
+    await handleConfiguration();
+    handleStreamerProcess(false);
+    handleServerProcess(false);
 }
 
 async function runServer() {
@@ -76,10 +207,25 @@ async function runServer() {
     await runFile(node, './scripts/plugins/update-dependencies');
     await handleConfiguration();
 
-    handleStreamerProcess();
-    handleServerProcess();
+    if (passedArguments.includes('--dev')) {
+        await devMode(true);
+        handleStreamerProcess(false);
+        handleServerProcess(false);
+        return;
+    }
+
+    handleStreamerProcess(true);
+    handleServerProcess(true);
 }
 
 if (passedArguments.includes('--start')) {
+    for (const port of ports) {
+        try {
+            fkill(port, { force: true, ignoreCase: true, silent: true });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     runServer();
 }
