658548be865e4255c38da3da1d986a79ee2f5d9e
stuyk <stuykgaming@gmail.com>
2022-06-21 14:29:07 -0600

    Fix nametag config

--- src/core/plugins/core-nametags/shared/src/config.ts
+++ src/core/plugins/core-nametags/shared/src/config.ts
@@ -1,12 +1,14 @@
 import { INametagConfig } from '../interfaces';
 
 // Hover over these in VSCode to see what they do.
-export default {
+const nametagConfig: INametagConfig = {
     MAX_DRAW_DISTANCE: 50,
     SHOULD_SCREEN_SHAKE: true,
     SHOW_NAME: true,
-    SHOW_ACCOUNT_ID: true,
+    SHOW_ID: true,
     SHOW_NAME_INSTANTLY: false,
     SHOW_NAMETAGS_WITH_KEY: false,
     NAME_TAGS_KEY: 18, // Left Alt
-} as INametagConfig;
+};
+
+export default nametagConfig;
