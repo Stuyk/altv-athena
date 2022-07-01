5428285cf24bb99ec1e414952401018676bf1e07
Dex <dex35803@gmail.com>
2022-07-01 09:27:47 -0400

    Add ability to inject various vue plugins directly at Vue app creation, this is totally dynamic and example is included (#249)

--- src-webviews/src/main.ts
+++ src-webviews/src/main.ts
@@ -1,5 +1,6 @@
 import { App, createApp } from 'vue';
 import ActualApp from './App.vue';
+import { VUE_PLUGIN_IMPORTS } from './plugins/vue-plugin-imports';
 
 const MOUNT_DIV_ID = '#app';
 let app: App<Element>;
@@ -7,6 +8,9 @@ let app: App<Element>;
 export class ComponentRegistration {
     static init() {
         app = createApp(ActualApp);
+        for (let i = 0; i < VUE_PLUGIN_IMPORTS.length; i++) {
+            app.use(VUE_PLUGIN_IMPORTS[i]);
+        }
         app.mount(MOUNT_DIV_ID);
     }
 }
---
+++ src/core/plugins/example-webview/webview/plugins/pinia.ts
@@ -0,0 +1,4 @@
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+
+export default pinia;
