ed6a86763087b944ed234bfde8d2f73a6d57f609
stuyk <stuykgaming@gmail.com>
2022-08-10 08:34:23 -0600

    Add CSS Folder to WebView Plugins

--- scripts/plugins/files.js
+++ scripts/plugins/files.js
@@ -2,6 +2,7 @@ import { movePluginFilesToWebview } from './shared.js';
 
 function run() {
     movePluginFilesToWebview('icons', ['png'])
+    movePluginFilesToWebview('webview/css', ['sass', 'css', 'scss', 'stylus'])
     movePluginFilesToWebview('webview/images', ['jpg', 'png', 'svg', 'jpeg', 'gif'])
     movePluginFilesToWebview('webview/videos', ['webm', 'avi'])
     movePluginFilesToWebview('sounds', ['ogg'])
