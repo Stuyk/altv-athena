b7fbd0974de8d157ad9c8c16f310f10a44b45a80
xLuxy <luxorion1905@gmail.com>
2022-08-21 11:06:20 +0200

    Add `KeyHeld.unregister` method

--- src/core/client/events/keyHeld.ts
+++ src/core/client/events/keyHeld.ts
@@ -58,6 +58,48 @@ export class KeyHeld {
 
         alt.log(`${String.fromCharCode(key)} | Was Registered in KeyHeld`);
     }
+
+    /**
+     * Use https://keycode.info for proper key numbers.
+     *
+     * @static
+     * @param {number} key
+     * @param {Function} onKeyDown Handle to the onKeyDown callback used in the register method call.
+     * @param {Function} onKeyUp Handle to the onKeyUp callback used in the register method call.
+     * @return {void}
+     * @memberof KeyHeld
+     */
+    static unregister(key: number, onKeyDown: Function, onKeyUp: Function): void {
+        // Verify a key is passed
+        if (typeof key === 'undefined') {
+            alt.logError(`Key was not specified for KeyHeld.unregister`);
+            return;
+        }
+
+        // Verify functions have values
+        if (typeof onKeyDown !== 'function' || typeof onKeyUp !== 'function') {
+            alt.logWarning(`${key} in KeyHeld.unregister does not have valid callback functions.`);
+            return;
+        }
+
+        if (!holdableKeys[key]) {
+            return;
+        }
+
+        const keyUpExists = holdableKeys[key].onKeyUp === onKeyUp;
+        const keyDownExists = holdableKeys[key].onKeyDown === onKeyDown;
+
+        if (keyUpExists && keyDownExists) {
+            delete holdableKeys[key];
+            return;
+        }
+
+        if (keyUpExists) {
+            delete holdableKeys[key].onKeyUp;
+        } else if (keyDownExists) {
+            delete holdableKeys[key].onKeyDown;
+        }
+    }
 }
 
 KeyHeld.init();
