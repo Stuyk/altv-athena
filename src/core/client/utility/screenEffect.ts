f16bd15982cc5d7d7194af1030db89a71a212ef5
MathieuGermain <gtavoverride@gmail.com>
2022-10-04 16:16:47 -0400

    Changed ScreenEffect class to const #299

--- src/core/client/utility/screenEffect.ts
+++ src/core/client/utility/screenEffect.ts
@@ -1,16 +1,16 @@
 import * as natives from "natives";
-import { SCREEN_EFFECTS } from "../../shared/enums/ScreenEffects";
+import { SCREEN_EFFECTS } from "../../shared/enums/screenEffects";
 
-export abstract class ScreenEffect {
+export const ScreenEffect = {
 
     /**
      * Check whether the specific screen effect is running.
      * @param {SCREEN_EFFECTS} screenEffect The ScreenEffect to check.
      * @return true if the screen effect is active; otherwise false
      */
-    public static isEffectActive(screenEffect: SCREEN_EFFECTS) {
-        return natives.animpostfxIsRunning(screenEffect)
-    }
+    isEffectActive(screenEffect: SCREEN_EFFECTS) {
+        return natives.animpostfxIsRunning(screenEffect);
+    },
 
     /**
      * Starts applying the specified effect to the screen.
@@ -18,23 +18,23 @@ export abstract class ScreenEffect {
      * @param {number} duration The duration of the effect in milliseconds or zero to use the default length.
      * @param {boolean} looped If true the effect won't stop until stopEffect(ScreenEffect) is called.
      */
-    public static startEffect(screenEffect: SCREEN_EFFECTS, duration = 0, looped = false) {
-        natives.animpostfxPlay(screenEffect, duration, looped)
-    }
+    startEffect(screenEffect: SCREEN_EFFECTS, duration = 0, looped = false) {
+        natives.animpostfxPlay(screenEffect, duration, looped);
+    },
 
     /**
      * Stops applying the specified effect to the screen.
      * @param {SCREEN_EFFECTS} screenEffect The ScreenEffect to stop playing.
      */
-    public static stopEffect(screenEffect: SCREEN_EFFECTS) {
-        natives.animpostfxStop(screenEffect)
-    }
+    stopEffect(screenEffect: SCREEN_EFFECTS) {
+        natives.animpostfxStop(screenEffect);
+    },
 
     /**
      * Stops all currently running effects.
      */
-    public static stopAllEffects() {
-        natives.animpostfxStopAll()
-    }
+    stopAllEffects() {
+        natives.animpostfxStopAll();
+    },
 
 }
