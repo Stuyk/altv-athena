be20959c3ee958f3dd07544e5d2de543764b1b30
MathieuGermain <gtavoverride@gmail.com>
2022-10-04 11:33:11 -0400

    Imported the enum & changed the parameter accordingly
    Using Three Steps by default cause the One Step can be buggy on high altitude location such as Mount Chiliad.

--- src/core/client/utility/switch.ts
+++ src/core/client/utility/switch.ts
@@ -1,14 +1,16 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
+import { SWITCHOUT_TYPES } from '../../shared/enums/switchOutTypes';
 import { sleep } from './sleep';
 
 /**
  * Zoom all the way out. Then zoom all the way in from the sky.
  * @export
  * @param {number} duration
+ * @param {SWITCHOUT_TYPES} switchType
  * @return {*}  {Promise<boolean>}
  */
-export async function switchInPlayer(duration: number, switchType: number = 2): Promise<boolean> {
+export async function switchInPlayer(duration: number, switchType: SWITCHOUT_TYPES = SWITCHOUT_TYPES.THREE_STEPS): Promise<boolean> {
     if (!native.isPlayerSwitchInProgress()) {
         native.switchOutPlayer(alt.Player.local.scriptID, 0, switchType);
     }
