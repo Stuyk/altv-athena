e3fc8edc18c1333350d8dacbf88cab5061dcb999
MathieuGermain <gtavoverride@gmail.com>
2022-10-05 16:36:48 -0400

    Changed the options

--- src/core/plugins/job-vehicle-repo/shared/options.ts
+++ src/core/plugins/job-vehicle-repo/shared/options.ts
@@ -1,6 +1,21 @@
 export const JOB_VEHICLE_REPO_OPTIONS = {
-    // Cooldown before the mission is available again
-    COOLDOWN: 60000,
-    //Maximum payout
-    MAX_PAYOUT: 1500,
+    /**
+     * Max time to complete the job in minutes
+     */
+    MAXTIME: 30,
+    /**
+     * Time in minutes before the job is available again
+     */
+    COOLDOWN: 1,
+    /**
+     * Maximum payout
+     * Cap the payout to a limit
+     */
+    MAX_PAYOUT: 1000,
+    /**
+     * Payout Distance Multiplier
+     * distance * multiplier = payout
+     * Example: 1000 meter * 0.5 = $500
+     */
+    PAYOUT_DISTANCE_MULTIPLIER: 0.5,
 };
