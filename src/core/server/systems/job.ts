2252fe15831c2bef099a2c6d10b5708668371436
MathieuGermain <gtavoverride@gmail.com>
2022-10-04 22:51:13 -0400

    Added an Async JobCompleted callback
    Allows us to do something before the cleanup.

--- src/core/server/systems/job.ts
+++ src/core/server/systems/job.ts
@@ -67,6 +67,7 @@ export class Job {
     private objectives: Array<Objective> = [];
     private vehicles: Array<alt.Vehicle> = [];
     private startTime: number;
+    private completedCallback: (job: Job) => Promise<void>;
     private quitCallback: (job: Job, reason: QuitReasons) => void;
 
     /**
@@ -203,6 +204,10 @@ export class Job {
     async checkObjective(): Promise<boolean> {
         const objective = this.getCurrentObjective();
 
+        if (!objective) {
+            return false;
+        }
+
         // Skip all default checks if this is set to true.
         if (!objective.onlyCallbackCheck) {
             const passedCritera = this.verifyCriteria(objective);
@@ -242,7 +247,7 @@ export class Job {
             Athena.player.emit.particle(this.player, objective.particle, true);
         }
 
-        this.goToNextObjective();
+        await this.goToNextObjective();
         return true;
     }
 
@@ -453,15 +458,27 @@ export class Job {
      * @private
      * @memberof JobBuilder
      */
-    goToNextObjective() {
+    async goToNextObjective() {
         const returnedObjective = this.objectives.shift();
         if (returnedObjective && returnedObjective.callbackOnFinish) {
             returnedObjective.callbackOnFinish(this.player);
         }
 
         if (this.objectives.length <= 0) {
+            /**
+             * We may want to do something async when the mission is completed.
+             * Before it is cleaned up.
+             */
+            if (typeof this.completedCallback === 'function') {
+                await this.completedCallback(this);
+            }
             this.removeAllVehicles();
             Athena.player.emit.message(this.player, `Job Completed`);
+
+            if (JobInstances[this.player.id]) {
+                delete JobInstances[this.player.id];
+            }
+
             alt.emitClient(this.player, JobEnums.ObjectiveEvents.JOB_SYNC, null);
             return;
         }
@@ -487,7 +504,8 @@ export class Job {
      */
     private tryAnimation() {
         const objective = this.getCurrentObjective();
-        if (!objective.animation) {
+
+        if (!objective || !objective.animation) {
             return;
         }
 
@@ -609,9 +627,19 @@ export class Job {
     }
 
     /**
+     * Adds an async callback that is called when a user complete a job.
+     *
+     * @param {(job: Job) => Promise<void>} callback
+     * @memberof Job
+     */
+    addCompletedCallback(callback: (job: Job) => Promise<void>) {
+        this.completedCallback = callback;
+    }
+
+    /**
      * Adds a callback that is called when a user quits a job.
      *
-     * @param {(job: Job) => {}} callback
+     * @param {(job: Job) => void} callback
      * @memberof Job
      */
     addQuitCallback(callback: (job: Job, reason: 'user' | 'disconnected') => void) {
