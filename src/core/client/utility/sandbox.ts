4dfac60b2faffa77da2e4041037958e49ef7be69
stuyk <stuykgaming@gmail.com>
2022-10-12 15:11:35 -0600

    Comment out sandbox

--- src/core/client/utility/sandbox.ts
+++ src/core/client/utility/sandbox.ts
@@ -1,44 +1,44 @@
-import * as alt from 'alt-client';
-import * as native from 'natives';
-import { SYSTEM_EVENTS } from '@AthenaShared/enums/system';
-import { AthenaClient } from '@AthenaClient/api/athena';
+// import * as alt from 'alt-client';
+// import * as native from 'natives';
+// import { SYSTEM_EVENTS } from '@AthenaShared/enums/system';
+// import { AthenaClient } from '@AthenaClient/api/athena';
 
-alt.onServer(SYSTEM_EVENTS.TICKS_START, async () => {
-    await alt.Utils.wait(2000);
-    await AthenaClient.rmlui.menu3D.create(
-        { x: -839.8507690429688, y: -151.79005432128906, z: 19.950349807739258 },
-        [
-            {
-                name: 'Orange Juice',
-                callback: () => {
-                    console.log('Selected Orange Juice!');
-                },
-            },
-            {
-                name: 'Coke',
-                callback: () => {
-                    console.log('Selected Coke!');
-                },
-            },
-            {
-                name: 'Pepsi',
-                callback: () => {
-                    console.log('Selected Pepsi!');
-                },
-            },
-            {
-                name: 'Mountain Dew',
-                callback: () => {
-                    console.log('Selected Mountain Dew!');
-                },
-            },
-            {
-                name: 'Energy Drank',
-                callback: () => {
-                    console.log('Selected Energy Drank!');
-                },
-            },
-        ],
-        10,
-    );
-});
+// alt.onServer(SYSTEM_EVENTS.TICKS_START, async () => {
+//     await alt.Utils.wait(2000);
+//     await AthenaClient.rmlui.menu3D.create(
+//         { x: -839.8507690429688, y: -151.79005432128906, z: 19.950349807739258 },
+//         [
+//             {
+//                 name: 'Orange Juice',
+//                 callback: () => {
+//                     console.log('Selected Orange Juice!');
+//                 },
+//             },
+//             {
+//                 name: 'Coke',
+//                 callback: () => {
+//                     console.log('Selected Coke!');
+//                 },
+//             },
+//             {
+//                 name: 'Pepsi',
+//                 callback: () => {
+//                     console.log('Selected Pepsi!');
+//                 },
+//             },
+//             {
+//                 name: 'Mountain Dew',
+//                 callback: () => {
+//                     console.log('Selected Mountain Dew!');
+//                 },
+//             },
+//             {
+//                 name: 'Energy Drank',
+//                 callback: () => {
+//                     console.log('Selected Energy Drank!');
+//                 },
+//             },
+//         ],
+//         10,
+//     );
+// });
