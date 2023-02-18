a0cbfe67b8e12bc19caf2b5ea86c9eeb45d34506
stuyk <stuykgaming@gmail.com>
2023-02-18 06:15:38 -0700

    Begin Vehicle Doc Migrations

--- src/core/server/documents/vehicle.ts
+++ src/core/server/documents/vehicle.ts
@@ -6,65 +6,108 @@ import { IVehicle } from '@AthenaShared/interfaces/iVehicle';
 type KeyChangeCallback = (vehicle: alt.Vehicle, newValue: any, oldValue: any) => void;
 
 const callbacks: { [key: string]: Array<KeyChangeCallback> } = {};
+const cache: { [id: string]: IVehicle } = {};
+const DEBUG_MODE = false;
 
 /**
- * Return current vehicle data and their associated vehicle object.
+ * Used to unbind vehicle cache for an id once the vehicle is deleted
+ *
+ * @param {number} id
+ */
+function unbind(id: number) {
+    delete cache[id];
+}
+
+/**
+ * Used to bind a vehicle document to a vehicle entity
+ *
+ * @param {alt.Vehicle} vehicle
+ * @param {IVehicle} document
+ */
+function bind(vehicle: alt.Vehicle, document: IVehicle) {
+    if (document._id) {
+        document._id = document._id.toString();
+    }
+
+    cache[vehicle.id] = document;
+}
+
+/**
+ * Get a vehicle document attached to a vehicle
  *
  * @template T
  * @param {alt.Vehicle} vehicle
- * @return {T = Vehicle}
+ * @return {(T | undefined)}
  */
 function get<T = IVehicle>(vehicle: alt.Vehicle): T | undefined {
-    if (typeof vehicle.data === 'undefined' || !vehicle.data._id) {
-        alt.logWarning(`Cannot fetch data for Vehicle (${vehicle.id}) it is not a document supported vehicle.`);
-        return undefined;
-    }
-
-    return vehicle.data as T;
+    return cache[vehicle.id] as T;
 }
 
 /**
- * Get the current value of a specific field inside of the vehicle data object.
- * Can be extended to obtain any value easily.
+ * Get a field directly for a vehicle document
  *
  * @template T
- * @param {alt.Vehicle} vehicle
- * @param {(keyof KnownKeys<Vehicle & T>)} fieldName
- * @return {*}
- */
-function getField<T = {}>(vehicle: alt.Vehicle, fieldName: keyof KnownKeys<IVehicle & T>) {
-    if (typeof vehicle.data === 'undefined' || !vehicle.data._id) {
-        alt.logWarning(`Cannot fetch data for Vehicle (${vehicle.id}) it is not a document supported vehicle.`);
-        return undefined;
-    }
-
-    return vehicle.data[String(fieldName)];
-}
-
-/**
- * Sets a vehicle document value, and saves it automatically to the selected vehicles's database.
- * Automatically calls all callbacks associated with the field name.
- *
- * @template T
+ * @template ReturnType
  * @param {alt.Vehicle} vehicle
  * @param {(keyof KnownKeys<IVehicle & T>)} fieldName
- * @param {*} value
- * @return {void}
+ * @return {(ReturnType | undefined)}
  */
-async function set<T = {}>(vehicle: alt.Vehicle, fieldName: keyof KnownKeys<IVehicle & T>, value: any) {
-    if (typeof vehicle.data === 'undefined' || !vehicle.data._id) {
-        alt.logWarning(`Cannot set data for Vehicle (${vehicle.id}) it is not a document supported vehicle.`);
+function getField<T = {}, ReturnType = any>(
+    vehicle: alt.Vehicle,
+    fieldName: keyof KnownKeys<IVehicle & T>,
+): ReturnType | undefined {
+    if (!cache[vehicle.id]) {
+        return undefined;
+    }
+
+    return cache[vehicle.id][String(fieldName)];
+}
+
+/**
+ * Set vehicle data for a given field, automatically saves to database.
+ *
+ * @template T
+ * @template Keys
+ * @param {alt.Vehicle} vehicle
+ * @param {Keys} fieldName
+ * @param {*} value
+ * @param {boolean} [skipCallbacks=false]
+ * @return {*}
+ */
+async function set<T = {}, Keys = keyof KnownKeys<IVehicle & T>>(
+    vehicle: alt.Vehicle,
+    fieldName: Keys,
+    value: any,
+    skipCallbacks = false,
+) {
+    if (!cache[vehicle.id]) {
         return undefined;
     }
 
     const typeSafeFieldName = String(fieldName);
-    const oldValue = JSON.parse(JSON.stringify(vehicle.data[typeSafeFieldName]));
+    let oldValue = undefined;
+    if (cache[vehicle.id][typeSafeFieldName]) {
+        oldValue = JSON.parse(JSON.stringify(cache[vehicle.id][typeSafeFieldName]));
+    }
+
     const newData = { [typeSafeFieldName]: value };
 
-    vehicle.data = Object.assign(vehicle.data, newData);
-    await Database.funcs.updatePartialData(vehicle.data._id, newData, Database.collections.Vehicles);
+    cache[vehicle.id] = Object.assign(cache[vehicle.id], newData);
+    await Database.funcs.updatePartialData(cache[vehicle.id]._id, newData, Database.collections.Vehicles);
+
+    if (DEBUG_MODE) {
+        alt.logWarning(
+            `DEBUG: ${cache[vehicle.id]._id} state updated for ${typeSafeFieldName} with value: ${JSON.stringify(
+                newData,
+            )}`,
+        );
+    }
 
     if (typeof callbacks[typeSafeFieldName] === 'undefined') {
+        return;
+    }
+
+    if (skipCallbacks) {
         return;
     }
 
@@ -74,36 +117,31 @@ async function set<T = {}>(vehicle: alt.Vehicle, fieldName: keyof KnownKeys<IVeh
 }
 
 /**
- * Sets vehicle document values, and saves it automatically to the selected vehicles's database.
- * Automatically calls all callbacks associated with the field name.
+ * Set bulk data for a vehicle document.
+ * Automatically saves to database.
  *
  * @template T
+ * @template Keys
  * @param {alt.Vehicle} vehicle
- * @param {(Partial<IVehicle & T>)} fields
- * @returns {void}
+ * @param {Keys} fields
  */
-async function setBulk<T = {}>(vehicle: alt.Vehicle, fields: Partial<IVehicle & T>) {
-    if (typeof vehicle.data === 'undefined' || !vehicle.data._id) {
-        alt.logWarning(`Cannot set bulk data for Vehicle (${vehicle.id}) it is not a document supported vehicle.`);
-        return undefined;
-    }
-
+async function setBulk<T = {}, Keys = Partial<IVehicle & T>>(vehicle: alt.Vehicle, fields: Keys) {
     const oldValues = {};
 
     Object.keys(fields).forEach((key) => {
-        oldValues[key] = JSON.parse(JSON.stringify(vehicle.data[key]));
+        oldValues[key] = JSON.parse(JSON.stringify(cache[vehicle.id][key]));
     });
 
-    vehicle.data = Object.assign(vehicle.data, fields);
-    await Database.funcs.updatePartialData(vehicle.data._id, fields, Database.collections.Vehicles);
+    cache[vehicle.id] = Object.assign(cache[vehicle.id], fields);
+    await Database.funcs.updatePartialData(cache[vehicle.id]._id, fields, Database.collections.Vehicles);
 
-    Object.keys(oldValues).forEach((key) => {
+    Object.keys(fields).forEach((key) => {
         if (typeof callbacks[key] === 'undefined') {
             return;
         }
 
         for (let cb of callbacks[key]) {
-            cb(vehicle, fields[key], oldValues[key]);
+            cb(vehicle, cache[vehicle.id][key], oldValues[key]);
         }
     });
 }
@@ -126,9 +164,19 @@ function onChange<T = {}>(fieldName: keyof KnownKeys<IVehicle & T>, callback: Ke
 }
 
 export const VehicleDocument = {
+    bind,
     get,
     getField,
+    onChange,
     set,
     setBulk,
-    onChange,
+    unbind,
 };
+
+alt.on('removeEntity', (entity: alt.Entity) => {
+    if (!(entity instanceof alt.Vehicle)) {
+        return;
+    }
+
+    unbind(entity.id);
+});
