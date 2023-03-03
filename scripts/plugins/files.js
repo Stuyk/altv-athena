7b81ff2a82d416dd9be19637838b6155f04ee076
stuyk <stuykgaming@gmail.com>
2023-03-03 11:07:56 -0700

    assets folder support

--- scripts/plugins/files.js
+++ scripts/plugins/files.js
@@ -1,10 +1,29 @@
-import { movePluginFilesToWebview } from './shared.js';
+import { moveAssetsToWebview, movePluginFilesToWebview, clearPluginsWebViewFolder } from './shared.js';
 
 function run() {
+    clearPluginsWebViewFolder();
     movePluginFilesToWebview('icons', ['png']);
     movePluginFilesToWebview('webview/images', ['jpg', 'png', 'svg', 'jpeg', 'gif']);
     movePluginFilesToWebview('webview/videos', ['webm', 'avi']);
     movePluginFilesToWebview('sounds', ['ogg']);
+    moveAssetsToWebview('webview/assets', [
+        'jpg',
+        'png',
+        'svg',
+        'jpeg',
+        'gif',
+        'webm',
+        'webp',
+        'avi',
+        'ogg',
+        'wav',
+        'css',
+        'html',
+        'js',
+        'ico',
+        'otf',
+        'ttf',
+    ]);
 }
 
 run();
