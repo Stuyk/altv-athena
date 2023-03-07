fbb8049319380c81b798e0859ecf2bd164f7c025
stuyk <stuykgaming@gmail.com>
2023-03-07 10:05:29 -0700

    Fix Job Vehicles

--- src/core/server/events/playerDeath.ts
+++ src/core/server/events/playerDeath.ts
@@ -1,7 +1,6 @@
 import * as alt from 'alt-server';
 
 import * as Athena from '@AthenaServer/api';
-import { VehicleSystem } from '../vehicle/vehicle';
 import * as PlayerEvents from '../player/events';
 
 function handleDeath(victim: alt.Player, killer: alt.Entity, weaponHash: any): void {
