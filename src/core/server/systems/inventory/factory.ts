ab9c6e8f94cca51fa1499fb19d727f43a60fe1c7
stuyk <stuykgaming@gmail.com>
2023-03-09 09:57:59 -0700

    Factory Overrides

--- src/core/server/systems/inventory/factory.ts
+++ src/core/server/systems/inventory/factory.ts
@@ -54,6 +54,10 @@ export async function getBaseItemAsync<CustomData = {}, CustomBehavior = {}>(
 ): Promise<BaseItem<DefaultItemBehavior & CustomBehavior, CustomData>> {
     await isDoneLoadingAsync();
 
+    if (Overrides.getBaseItemAsync) {
+        return await Overrides.getBaseItemAsync<CustomData, CustomBehavior>(dbName, version);
+    }
+
     const index = databaseItems.findIndex((item) => {
         const hasMatchingName = item.dbName === dbName;
 
@@ -88,6 +92,10 @@ export async function getBaseItemAsync<CustomData = {}, CustomBehavior = {}>(
  */
 export async function upsertAsync(baseItem: BaseItem) {
     await isDoneLoadingAsync();
+
+    if (Overrides.upsertAsync) {
+        return await Overrides.upsertAsync(baseItem);
+    }
 
     const index = databaseItems.findIndex((item) => {
         const hasMatchingName = item.dbName === baseItem.dbName;
@@ -139,6 +147,10 @@ export async function fromStoredItemAsync<CustomData = {}, CustomBehavior = {}>(
 ): Promise<Item<CustomBehavior & DefaultItemBehavior, CustomData> | undefined> {
     await isDoneLoadingAsync();
 
+    if (Overrides.fromStoredItemAsync) {
+        return await Overrides.fromStoredItemAsync<CustomData, CustomBehavior>(item);
+    }
+
     const baseItem = await getBaseItemAsync<CustomData, CustomBehavior>(item.dbName, item.version);
     if (typeof baseItem === 'undefined') {
         return undefined;
@@ -166,6 +178,10 @@ export async function toStoredItemAsync<CustomData = {}>(
     item: Item<DefaultItemBehavior, CustomData>,
 ): Promise<StoredItem<CustomData>> {
     await isDoneLoadingAsync();
+
+    if (Overrides.toStoredItem) {
+        return await Overrides.toStoredItem<CustomData>(item);
+    }
 
     const storedItem: StoredItem<CustomData> = {
         dbName: item.dbName,
@@ -199,6 +215,10 @@ export async function fromBaseToStoredAsync<CustomData = {}>(
     quantity: number,
 ): Promise<StoredItem<CustomData>> {
     await isDoneLoadingAsync();
+
+    if (Overrides.fromBaseToStoredAsync) {
+        return await Overrides.fromBaseToStoredAsync<CustomData>(baseItem, quantity);
+    }
 
     const storedItem: StoredItem<CustomData> = {
         dbName: baseItem.dbName,
@@ -235,6 +255,10 @@ export function getBaseItem<CustomData = {}, CustomBehavior = {}>(
     dbName: string,
     version: number = undefined,
 ): BaseItem<DefaultItemBehavior & CustomBehavior, CustomData> {
+    if (Overrides.getBaseItem) {
+        return Overrides.getBaseItem<CustomData, CustomBehavior>(dbName, version);
+    }
+
     const index = databaseItems.findIndex((item) => {
         const hasMatchingName = item.dbName === dbName;
 
@@ -257,6 +281,7 @@ export function getBaseItem<CustomData = {}, CustomBehavior = {}>(
 
     return deepCloneObject<BaseItem<DefaultItemBehavior & CustomBehavior, CustomData>>(databaseItems[index]);
 }
+
 /**
  * Converts an item from a player inventory, or toolbar to a full item set.
  *
@@ -272,6 +297,10 @@ export function getBaseItem<CustomData = {}, CustomBehavior = {}>(
 export function fromStoredItem<CustomData = {}, CustomBehavior = DefaultItemBehavior>(
     item: StoredItem<CustomData>,
 ): Item<CustomBehavior & DefaultItemBehavior, CustomData> | undefined {
+    if (Overrides.fromStoredItem) {
+        return Overrides.fromStoredItem<CustomData, CustomBehavior>(item);
+    }
+
     const baseItem = getBaseItem<CustomData, CustomBehavior>(item.dbName, item.version);
     if (typeof baseItem === 'undefined') {
         return undefined;
@@ -285,6 +314,7 @@ export function fromStoredItem<CustomData = {}, CustomBehavior = DefaultItemBeha
 
     return combinedItem;
 }
+
 /**
  * Converts a full item, into a storeable version of the item.
  *
@@ -297,6 +327,10 @@ export function fromStoredItem<CustomData = {}, CustomBehavior = DefaultItemBeha
  * @return {StoredItem<CustomData>}
  */
 export function toStoredItem<CustomData = {}>(item: Item<DefaultItemBehavior, CustomData>): StoredItem<CustomData> {
+    if (Overrides.toStoredItem) {
+        return Overrides.toStoredItem<CustomData>(item);
+    }
+
     const storedItem: StoredItem<CustomData> = {
         dbName: item.dbName,
         data: item.data,
@@ -328,6 +362,10 @@ export function fromBaseToStored<CustomData = {}>(
     baseItem: BaseItem<DefaultItemBehavior, CustomData>,
     quantity: number,
 ) {
+    if (Overrides.fromBaseToStored) {
+        return Overrides.fromBaseToStored(baseItem, quantity);
+    }
+
     const storedItem: StoredItem<CustomData> = {
         dbName: baseItem.dbName,
         data: baseItem.data,
@@ -344,6 +382,38 @@ export function fromBaseToStored<CustomData = {}>(
     }
 
     return storedItem;
+}
+
+interface FactoryFuncs {
+    getBaseItemAsync: typeof getBaseItemAsync;
+    upsertAsync: typeof upsertAsync;
+    fromStoredItemAsync: typeof fromStoredItemAsync;
+    fromBaseToStoredAsync: typeof fromBaseToStoredAsync;
+    getBaseItem: typeof getBaseItem;
+    fromStoredItem: typeof fromStoredItem;
+    toStoredItem: typeof toStoredItem;
+    fromBaseToStored: typeof fromBaseToStored;
+}
+
+const Overrides: Partial<FactoryFuncs> = {};
+
+export function override(functionName: 'getBaseItemAsync', callback: typeof getBaseItemAsync);
+export function override(functionName: 'upsertAsync', callback: typeof upsertAsync);
+export function override(functionName: 'fromStoredItemAsync', callback: typeof fromStoredItemAsync);
+export function override(functionName: 'fromBaseToStoredAsync', callback: typeof fromBaseToStoredAsync);
+export function override(functionName: 'getBaseItem', callback: typeof getBaseItem);
+export function override(functionName: 'fromStoredItem', callback: typeof fromStoredItem);
+export function override(functionName: 'toStoredItem', callback: typeof toStoredItem);
+export function override(functionName: 'fromBaseToStored', callback: typeof fromBaseToStored);
+/**
+ * Used to override inventory item effects functionality
+ *
+ * @export
+ * @param {keyof FactoryFuncs} functionName
+ * @param {*} callback
+ */
+export function override(functionName: keyof FactoryFuncs, callback: any): void {
+    Overrides[functionName] = callback;
 }
 
 InternalFunctions.init();
