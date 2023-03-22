3e3ade1dbd8bc5b458c3d2075e2a10897e487df5
stuyk <stuykgaming@gmail.com>
2023-03-21 18:05:10 -0600

    Expose Random Functions to API

--- src/core/client/utility/index.ts
+++ src/core/client/utility/index.ts
@@ -2,6 +2,7 @@ export { DirectionVector } from './directionToVector';
 export * as math from './math';
 export * as model from './model';
 export * as pauseMenu from './pauseMenu';
+export * as random from '@AthenaShared/utility/random';
 export * as scene from './scene';
 export * as scenarios from './scenarios';
 export * as vector from '@AthenaShared/utility/vector';
