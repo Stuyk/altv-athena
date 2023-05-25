16c490d86262509db482dd3b995de1d4adc2da89
Stuyk <stuykgaming@gmail.com>
2023-05-25 08:44:26 -0600

    Despawn Patch

--- src/core/server/document/vehicle.ts
+++ src/core/server/document/vehicle.ts
@@ -234,7 +234,7 @@ export function getAllOnline<T = {}>(): Array<{ id: number; document: OwnedVehic
  */
 export function exists(_id: string): boolean {
     const sessions = Athena.session.vehicle.getAll(SessionKey);
-    return sessions.find((x) => x._id === _id) ? true : false;
+    return sessions.find((x) => x._id.toString() === _id.toString()) ? true : false;
 }
 
 alt.on('removeEntity', (entity: alt.Entity) => {
