60d3b4b54c2dca7b374bbd558664c43f183e189b
Pablo <pablo-kornblum@outlook.com>
2023-12-28 21:01:09 +0100

    add boneInterface and pedBones list

---
+++ src/core/shared/information/bones.ts
@@ -0,0 +1,494 @@
+import { IBone } from '@AthenaShared/interfaces/bone.js';
+
+export const pedBones: Array<IBone> = [
+    {
+        boneName: 'SKEL_ROOT',
+        boneID: 0,
+        boneIndex: 0,
+    },
+    {
+        boneName: 'FB_R_Brow_Out_000',
+        boneID: 1356,
+        boneIndex: 111,
+    },
+    {
+        boneName: 'SKEL_L_Toe0',
+        boneID: 2108,
+        boneIndex: 5,
+    },
+    {
+        boneName: 'MH_R_Elbow',
+        boneID: 2992,
+        boneIndex: 96,
+    },
+    {
+        boneName: 'SKEL_L_Finger01',
+        boneID: 4089,
+        boneIndex: 44,
+    },
+    {
+        boneName: 'SKEL_L_Finger02',
+        boneID: 4090,
+        boneIndex: 45,
+    },
+    {
+        boneName: 'SKEL_L_Finger31',
+        boneID: 4137,
+        boneIndex: 56,
+    },
+    {
+        boneName: 'SKEL_L_Finger32',
+        boneID: 4138,
+        boneIndex: 57,
+    },
+    {
+        boneName: 'SKEL_L_Finger41',
+        boneID: 4153,
+        boneIndex: 59,
+    },
+    {
+        boneName: 'SKEL_L_Finger42',
+        boneID: 4154,
+        boneIndex: 60,
+    },
+    {
+        boneName: 'SKEL_L_Finger11',
+        boneID: 4169,
+        boneIndex: 49,
+    },
+    {
+        boneName: 'SKEL_L_Finger12',
+        boneID: 4170,
+        boneIndex: 50,
+    },
+    {
+        boneName: 'SKEL_L_Finger21',
+        boneID: 4185,
+        boneIndex: 53,
+    },
+    {
+        boneName: 'SKEL_L_Finger22',
+        boneID: 4186,
+        boneIndex: 54,
+    },
+    {
+        boneName: 'RB_L_ArmRoll',
+        boneID: 5232,
+        boneIndex: 66,
+    },
+    {
+        boneName: 'IK_R_Hand',
+        boneID: 6286,
+        boneIndex: 91,
+    },
+    {
+        boneName: 'RB_R_ThighRoll',
+        boneID: 6442,
+        boneIndex: 27,
+    },
+    {
+        boneName: 'SKEL_R_Clavicle',
+        boneID: 10706,
+        boneIndex: 68,
+    },
+    {
+        boneName: 'FB_R_Lip_Corner_000',
+        boneID: 11174,
+        boneIndex: 112,
+    },
+    {
+        boneName: 'SKEL_Pelvis',
+        boneID: 11816,
+        boneIndex: 1,
+    },
+    {
+        boneName: 'IK_Head',
+        boneID: 12844,
+        boneIndex: 99,
+    },
+    {
+        boneName: 'SKEL_L_Foot',
+        boneID: 14201,
+        boneIndex: 4,
+    },
+    {
+        boneName: 'MH_R_Knee',
+        boneID: 16335,
+        boneIndex: 23,
+    },
+    {
+        boneName: 'FB_LowerLipRoot_000',
+        boneID: 17188,
+        boneIndex: 119,
+    },
+    {
+        boneName: 'FB_R_Lip_Top_000',
+        boneID: 17719,
+        boneIndex: 117,
+    },
+    {
+        boneName: 'SKEL_L_Hand',
+        boneID: 18905,
+        boneIndex: 42,
+    },
+    {
+        boneName: 'FB_R_CheekBone_000',
+        boneID: 19336,
+        boneIndex: 110,
+    },
+    {
+        boneName: 'FB_UpperLipRoot_000',
+        boneID: 20178,
+        boneIndex: 114,
+    },
+    {
+        boneName: 'FB_L_Lip_Top_000',
+        boneID: 20279,
+        boneIndex: 116,
+    },
+    {
+        boneName: 'FB_LowerLip_000',
+        boneID: 20623,
+        boneIndex: 120,
+    },
+    {
+        boneName: 'SKEL_R_Toe0',
+        boneID: 20781,
+        boneIndex: 17,
+    },
+    {
+        boneName: 'FB_L_CheekBone_000',
+        boneID: 21550,
+        boneIndex: 106,
+    },
+    {
+        boneName: 'MH_L_Elbow',
+        boneID: 22711,
+        boneIndex: 67,
+    },
+    {
+        boneName: 'SKEL_Spine0',
+        boneID: 23553,
+        boneIndex: 35,
+    },
+    {
+        boneName: 'RB_L_ThighRoll',
+        boneID: 23639,
+        boneIndex: 26,
+    },
+    {
+        boneName: 'PH_R_Foot',
+        boneID: 24806,
+        boneIndex: 19,
+    },
+    {
+        boneName: 'SKEL_Spine1',
+        boneID: 24816,
+        boneIndex: 36,
+    },
+    {
+        boneName: 'SKEL_Spine2',
+        boneID: 24817,
+        boneIndex: 37,
+    },
+    {
+        boneName: 'SKEL_Spine3',
+        boneID: 24818,
+        boneIndex: 38,
+    },
+    {
+        boneName: 'FB_L_Eye_000',
+        boneID: 25260,
+        boneIndex: 105,
+    },
+    {
+        boneName: 'SKEL_L_Finger00',
+        boneID: 26610,
+        boneIndex: 43,
+    },
+    {
+        boneName: 'SKEL_L_Finger10',
+        boneID: 26611,
+        boneIndex: 48,
+    },
+    {
+        boneName: 'SKEL_L_Finger20',
+        boneID: 26612,
+        boneIndex: 52,
+    },
+    {
+        boneName: 'SKEL_L_Finger30',
+        boneID: 26613,
+        boneIndex: 55,
+    },
+    {
+        boneName: 'SKEL_L_Finger40',
+        boneID: 26614,
+        boneIndex: 58,
+    },
+    {
+        boneName: 'FB_R_Eye_000',
+        boneID: 27474,
+        boneIndex: 109,
+    },
+    {
+        boneName: 'SKEL_R_Forearm',
+        boneID: 28252,
+        boneIndex: 70,
+    },
+    {
+        boneName: 'PH_R_Hand',
+        boneID: 28422,
+        boneIndex: 90,
+    },
+    {
+        boneName: 'FB_L_Lip_Corner_000',
+        boneID: 29868,
+        boneIndex: 107,
+    },
+    {
+        boneName: 'SKEL_Head',
+        boneID: 31086,
+        boneIndex: 98,
+    },
+    {
+        boneName: 'IK_R_Foot',
+        boneID: 35502,
+        boneIndex: 18,
+    },
+    {
+        boneName: 'RB_Neck_1',
+        boneID: 35731,
+        boneIndex: 124,
+    },
+    {
+        boneName: 'IK_L_Hand',
+        boneID: 36029,
+        boneIndex: 62,
+    },
+    {
+        boneName: 'SKEL_R_Calf',
+        boneID: 36864,
+        boneIndex: 15,
+    },
+    {
+        boneName: 'RB_R_ArmRoll',
+        boneID: 37119,
+        boneIndex: 95,
+    },
+    {
+        boneName: 'FB_Brow_Centre_000',
+        boneID: 37193,
+        boneIndex: 113,
+    },
+    {
+        boneName: 'SKEL_Neck_1',
+        boneID: 39317,
+        boneIndex: 97,
+    },
+    {
+        boneName: 'SKEL_R_UpperArm',
+        boneID: 40269,
+        boneIndex: 69,
+    },
+    {
+        boneName: 'FB_R_Lid_Upper_000',
+        boneID: 43536,
+        boneIndex: 108,
+    },
+    {
+        boneName: 'RB_R_ForeArmRoll',
+        boneID: 43810,
+        boneIndex: 94,
+    },
+    {
+        boneName: 'SKEL_L_UpperArm',
+        boneID: 45509,
+        boneIndex: 40,
+    },
+    {
+        boneName: 'FB_L_Lid_Upper_000',
+        boneID: 45750,
+        boneIndex: 104,
+    },
+    {
+        boneName: 'MH_L_Knee',
+        boneID: 46078,
+        boneIndex: 11,
+    },
+    {
+        boneName: 'FB_Jaw_000',
+        boneID: 46240,
+        boneIndex: 118,
+    },
+    {
+        boneName: 'FB_L_Lip_Bot_000',
+        boneID: 47419,
+        boneIndex: 121,
+    },
+    {
+        boneName: 'FB_Tongue_000',
+        boneID: 47495,
+        boneIndex: 123,
+    },
+    {
+        boneName: 'FB_R_Lip_Bot_000',
+        boneID: 49979,
+        boneIndex: 122,
+    },
+    {
+        boneName: 'SKEL_R_Thigh',
+        boneID: 51826,
+        boneIndex: 14,
+    },
+    {
+        boneName: 'SKEL_R_Foot',
+        boneID: 52301,
+        boneIndex: 16,
+    },
+    {
+        boneName: 'IK_Root',
+        boneID: 56604,
+        boneIndex: 127,
+    },
+    {
+        boneName: 'SKEL_R_Hand',
+        boneID: 57005,
+        boneIndex: 71,
+    },
+    {
+        boneName: 'SKEL_Spine_Root',
+        boneID: 57597,
+        boneIndex: 34,
+    },
+    {
+        boneName: 'PH_L_Foot',
+        boneID: 57717,
+        boneIndex: 7,
+    },
+    {
+        boneName: 'SKEL_L_Thigh',
+        boneID: 58271,
+        boneIndex: 2,
+    },
+    {
+        boneName: 'FB_L_Brow_Out_000',
+        boneID: 58331,
+        boneIndex: 103,
+    },
+    {
+        boneName: 'SKEL_R_Finger00',
+        boneID: 58866,
+        boneIndex: 72,
+    },
+    {
+        boneName: 'SKEL_R_Finger10',
+        boneID: 58867,
+        boneIndex: 77,
+    },
+    {
+        boneName: 'SKEL_R_Finger20',
+        boneID: 58868,
+        boneIndex: 81,
+    },
+    {
+        boneName: 'SKEL_R_Finger30',
+        boneID: 58869,
+        boneIndex: 84,
+    },
+    {
+        boneName: 'SKEL_R_Finger40',
+        boneID: 58870,
+        boneIndex: 87,
+    },
+    {
+        boneName: 'PH_L_Hand',
+        boneID: 60309,
+        boneIndex: 61,
+    },
+    {
+        boneName: 'RB_L_ForeArmRoll',
+        boneID: 61007,
+        boneIndex: 65,
+    },
+    {
+        boneName: 'SKEL_L_Forearm',
+        boneID: 61163,
+        boneIndex: 41,
+    },
+    {
+        boneName: 'FB_UpperLip_000',
+        boneID: 61839,
+        boneIndex: 115,
+    },
+    {
+        boneName: 'SKEL_L_Calf',
+        boneID: 63931,
+        boneIndex: 3,
+    },
+    {
+        boneName: 'SKEL_R_Finger01',
+        boneID: 64016,
+        boneIndex: 73,
+    },
+    {
+        boneName: 'SKEL_R_Finger02',
+        boneID: 64017,
+        boneIndex: 74,
+    },
+    {
+        boneName: 'SKEL_R_Finger31',
+        boneID: 64064,
+        boneIndex: 85,
+    },
+    {
+        boneName: 'SKEL_R_Finger32',
+        boneID: 64065,
+        boneIndex: 86,
+    },
+    {
+        boneName: 'SKEL_R_Finger41',
+        boneID: 64080,
+        boneIndex: 88,
+    },
+    {
+        boneName: 'SKEL_R_Finger42',
+        boneID: 64081,
+        boneIndex: 89,
+    },
+    {
+        boneName: 'SKEL_R_Finger11',
+        boneID: 64096,
+        boneIndex: 78,
+    },
+    {
+        boneName: 'SKEL_R_Finger12',
+        boneID: 64097,
+        boneIndex: 79,
+    },
+    {
+        boneName: 'SKEL_R_Finger21',
+        boneID: 64112,
+        boneIndex: 82,
+    },
+    {
+        boneName: 'SKEL_R_Finger22',
+        boneID: 64113,
+        boneIndex: 83,
+    },
+    {
+        boneName: 'SKEL_L_Clavicle',
+        boneID: 64729,
+        boneIndex: 39,
+    },
+    {
+        boneName: 'FACIAL_facialRoot',
+        boneID: 65068,
+        boneIndex: 102,
+    },
+    {
+        boneName: 'IK_L_Foot',
+        boneID: 65245,
+        boneIndex: 6,
+    },
+];
